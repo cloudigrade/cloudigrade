5174cdffaba47d5cd628720e3fc393b972091551
Brad Smith <brasmith@redhat.com>
2018-02-13 10:16:50 -0500

    break out tests specifically for util.aws

--- cloudigrade/instance/management/commands/check_instances.py
+++ cloudigrade/instance/management/commands/check_instances.py
@@ -1,46 +1,12 @@
 """Management command for checking currently-running AWS EC2 instances."""
-import collections
 import logging
 
-import boto3
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext as _
 
 from util import aws
 
 logger = logging.getLogger(__name__)
-
-
-def get_running_instances(arn):
-    """
-    Find all running EC2 instances visible to the given ARN.
-
-    Args:
-        arn: Amazon Resource Name to use for assuming a role.
-
-    Returns:
-        dict: Lists of instance IDs keyed by region where they were found.
-
-    """
-    credentials = aws.get_credentials_for_arn(arn)
-    found_instances = collections.defaultdict(list)
-
-    for region_name in aws.get_regions():
-        role_session = boto3.Session(
-            aws_access_key_id=credentials['AccessKeyId'],
-            aws_secret_access_key=credentials['SecretAccessKey'],
-            aws_session_token=credentials['SessionToken'],
-            region_name=region_name,
-        )
-
-        ec2 = role_session.client('ec2')
-        logger.debug(_(f'Describing instances in {region_name} for {arn}'))
-        instances = ec2.describe_instances()
-        for reservation in instances.get('Reservations', []):
-            for instance in reservation.get('Instances', []):
-                found_instances[region_name].append(instance['InstanceId'])
-
-    return dict(found_instances)
 
 
 class Command(BaseCommand):
@@ -61,5 +27,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Handle command to display a list of running instances."""
         arn = options['arn']
-        found_instances = get_running_instances(arn)
+        found_instances = aws.get_running_instances(arn)
         self.stdout.write(_(f'Instances found include: {found_instances}'))
--- cloudigrade/util/aws.py
+++ cloudigrade/util/aws.py
@@ -1,5 +1,11 @@
 """Helper utility module to wrap up common AWS operations."""
+import collections
+import logging
+
 import boto3
+from django.utils.translation import gettext as _
+
+logger = logging.getLogger(__name__)
 
 
 def get_regions(service_name='ec2'):
@@ -16,21 +22,53 @@ def get_regions(service_name='ec2'):
     return boto3.Session().get_available_regions(service_name)
 
 
-def get_credentials_for_arn(arn, session_name='temp-session'):
+def get_credentials_for_arn(arn):
     """
     Get the credentials for a given ARN.
 
     Args:
         arn (str): Amazon Resource Name to use for assuming a role.
-        session_name (str): Optional custom name for the session.
 
     Returns:
         dict: The credentials we can use to establish a session.
 
     """
     sts = boto3.client('sts')
+    session_name = 'temp-session'  # TODO Does this need to be a funciton arg?
     assume_role_object = sts.assume_role(
         RoleArn=arn,
         RoleSessionName=session_name,
     )
     return assume_role_object['Credentials']
+
+
+def get_running_instances(arn):
+    """
+    Find all running EC2 instances visible to the given ARN.
+
+    Args:
+        arn (str): Amazon Resource Name to use for assuming a role.
+
+    Returns:
+        dict: Lists of instance IDs keyed by region where they were found.
+
+    """
+    credentials = get_credentials_for_arn(arn)
+    found_instances = collections.defaultdict(list)
+
+    for region_name in get_regions():
+        role_session = boto3.Session(
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
+            region_name=region_name,
+        )
+
+        ec2 = role_session.client('ec2')
+        logger.debug(_(f'Describing instances in {region_name} for {arn}'))
+        instances = ec2.describe_instances()
+        for reservation in instances.get('Reservations', []):
+            for instance in reservation.get('Instances', []):
+                found_instances[region_name].append(instance['InstanceId'])
+
+    return dict(found_instances)
