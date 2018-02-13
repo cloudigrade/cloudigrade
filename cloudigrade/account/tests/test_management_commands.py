35e9da97525d7552eafe5d62575c0f15dfc5f0ae
Brad Smith <brasmith@redhat.com>
2018-02-13 13:45:23 -0500

    check for running instances when adding an account
    This effectively collapses all of the functionality from the ``instance`` app into the ``account`` app.

--- cloudigrade/account/tests/test_management_commands.py
+++ cloudigrade/account/tests/test_management_commands.py
@@ -1,11 +1,15 @@
 """Collection of tests for Account management commands."""
+import uuid
 from io import StringIO
+from unittest import mock
+
 from django.core.management import call_command
 from django.utils.translation import gettext as _
 from django.test import TestCase
 
-
+from account.management.commands import add_account
 from account.models import Account
+from util.tests import helper
 
 
 class AddAccountTest(TestCase):
@@ -14,11 +18,24 @@ class AddAccountTest(TestCase):
     def test_command_output(self):
         """Test saving and processing of a test ARN."""
         out = StringIO()
-        test_arn = 'arn:aws:iam::123456789012:role/test_role_210987654321'
-        test_id = 123456789012
 
-        call_command('add_account', test_arn, stdout=out)
-        self.assertIn(_('ARN Info Stored'), out.getvalue())
-        account = Account.objects.get(account_id=test_id)
-        self.assertEqual(test_id, account.account_id)
-        self.assertEqual(test_arn, account.account_arn)
+        mock_account_id = helper.generate_dummy_aws_account_id()
+        mock_arn = helper.generate_dummy_arn(mock_account_id)
+        mock_instances = {
+            f'region-{uuid.uuid4()}': [str(uuid.uuid4())],
+        }
+
+        expected_instances = _(f'Instances found include: {mock_instances}')
+        expected_account = _('ARN Info Stored')
+
+        with mock.patch.object(add_account, 'aws') as mock_aws:
+            mock_aws.get_running_instances.return_value = mock_instances
+            call_command('add_account', mock_arn, stdout=out)
+
+        actual_stdout = out.getvalue()
+        self.assertIn(expected_account, actual_stdout)
+        self.assertIn(expected_instances, actual_stdout)
+
+        account = Account.objects.get(account_id=mock_account_id)
+        self.assertEqual(mock_account_id, account.account_id)
+        self.assertEqual(mock_arn, account.account_arn)
--- cloudigrade/util/tests/helper.py
+++ cloudigrade/util/tests/helper.py
@@ -1,4 +1,5 @@
 """Helper functions for generating test data."""
+import decimal
 import random
 
 import faker
@@ -8,11 +9,20 @@ MAX_AWS_ACCOUNT_ID = 999999999999
 
 def generate_dummy_aws_account_id():
     """Generate a dummy AWS Account ID for testing purposes."""
-    return str(random.randrange(MAX_AWS_ACCOUNT_ID))
+    return decimal.Decimal(random.randrange(MAX_AWS_ACCOUNT_ID))
 
 
 def generate_dummy_arn(account_id=None):
-    """Generate a dummy AWS ARN for testing purposes."""
+    """
+    Generate a dummy AWS ARN for testing purposes.
+
+    Args:
+        account_id (int): Optional Account ID. Default is randomly generated.
+
+    Returns:
+        str: A well-formed, randomized ARN.
+
+    """
     if account_id is None:
         account_id = generate_dummy_aws_account_id()
     words = faker.Faker().name().replace(' ', '_')
