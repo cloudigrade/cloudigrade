db414db189ca104151b8fc0ec2ebbb9f4c0a94dd
Andrew Berglund <aberglun@redhat.com>
2018-02-15 12:01:54 -0500

    Using flake8 noqa instead of forcuing usage of variable

--- cloudigrade/util/tests/test_aws.py
+++ cloudigrade/util/tests/test_aws.py
@@ -128,10 +128,9 @@ class UtilAwsTest(TestCase):
         }
 
         with patch.object(aws, 'get_credentials_for_arn') as mock_get_creds, \
-                patch.object(aws, 'boto3') as mock_boto3:
+                patch.object(aws, 'boto3') as __:  # noqa: F841
             mock_get_creds.return_value = mock_credentials
-            mock_client = mock_boto3.Session.return_value.client.return_value
-            mock_client.describe_instances.return_value = {}
+
             actual_verified = aws.verify_account_access(mock_arn)
 
         self.assertTrue(actual_verified)
