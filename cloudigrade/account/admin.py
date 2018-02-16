09fb54c62fb1a928b2340f8473cbda761c3a9c80
Brad Smith <brasmith@redhat.com>
2018-02-16 15:44:31 -0500

    register account models for admin use

---
+++ cloudigrade/account/admin.py
@@ -0,0 +1,7 @@
+"""Module to expose our models in Django admin."""
+from django.contrib import admin
+from account.models import Account, Instance, InstanceEvent
+
+admin.site.register(Account, admin.ModelAdmin)
+admin.site.register(Instance, admin.ModelAdmin)
+admin.site.register(InstanceEvent, admin.ModelAdmin)
