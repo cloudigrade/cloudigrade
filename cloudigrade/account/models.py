21fc6d17035fe1816104ae6cbf11c1d14b752f55
Brad Smith <brasmith@redhat.com>
2018-02-19 15:09:49 -0500

    add ec2 instance type attribute

--- cloudigrade/account/models.py
+++ cloudigrade/account/models.py
@@ -65,3 +65,4 @@ class InstanceEvent(BaseModel):
     occurred_at = models.DateTimeField(null=False)
     subnet = models.CharField(max_length=16, null=False, blank=False)
     ec2_ami_id = models.CharField(max_length=256, null=False, blank=False)
+    instance_type = models.CharField(max_length=64, null=False, blank=False)
