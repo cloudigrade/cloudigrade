ccf974d80fe0262960b5abc80a11fe205280e849
Ilya White <ilya.white@redhat.com>
2018-02-20 16:00:36 -0500

    Check to make sure that all required policy actions are allowed.
    * Update session handling
    * Update how other methods get a session
    * Pull in test dependencies in local requirements file

--- cloudigrade/util/tests/test_aws.py
+++ cloudigrade/util/tests/test_aws.py
@@ -1,4 +1,5 @@
 """Collection of tests for ``util.aws`` module."""
+import json
 import uuid
 from unittest.mock import patch
 
@@ -7,6 +8,7 @@ import faker
 from django.test import TestCase
 
 from util import aws
+from util.aws import extract_account_id_from_arn
 from util.tests import helper
 
 
@@ -36,7 +38,7 @@ class UtilAwsTest(TestCase):
         with patch.object(aws, 'boto3') as mock_boto3:
             mock_session = mock_boto3.Session.return_value
             mock_session.get_available_regions.return_value = mock_regions
-            actual_regions = aws.get_regions()
+            actual_regions = aws.get_regions(mock_session)
             self.assertTrue(mock_session.get_available_regions.called)
             mock_session.get_available_regions.assert_called_with('ec2')
         self.assertListEqual(mock_regions, actual_regions)
@@ -51,14 +53,15 @@ class UtilAwsTest(TestCase):
         with patch.object(aws, 'boto3') as mock_boto3:
             mock_session = mock_boto3.Session.return_value
             mock_session.get_available_regions.return_value = mock_regions
-            actual_regions = aws.get_regions('tng')
+            actual_regions = aws.get_regions(mock_session, 'tng')
             self.assertTrue(mock_session.get_available_regions.called)
             mock_session.get_available_regions.assert_called_with('tng')
         self.assertListEqual(mock_regions, actual_regions)
 
-    def test_get_credentials_for_arn(self):
-        """Assert get_credentials_for_arn returns credentials dict."""
+    def test_get_session(self):
+        """Assert get_session returns session object."""
         mock_arn = helper.generate_dummy_arn()
+        mock_account_id = extract_account_id_from_arn(mock_arn)
         mock_role = {
             'Credentials': {
                 'AccessKeyId': str(uuid.uuid4()),
@@ -68,27 +71,38 @@ class UtilAwsTest(TestCase):
             'foo': 'bar',
         }
 
-        with patch.object(aws, 'boto3') as mock_boto3:
-            mock_client = mock_boto3.client.return_value
-            mock_assume_role = mock_client.assume_role
+        with patch.object(aws.boto3, 'client') as mock_client:
+            mock_assume_role = mock_client.return_value.assume_role
             mock_assume_role.return_value = mock_role
 
-            actual_credentials = aws.get_credentials_for_arn(mock_arn)
+            session = aws.get_session(mock_arn)
+            creds = session.get_credentials().get_frozen_credentials()
 
-            mock_boto3.client.assert_called_with('sts')
-            mock_assume_role.assert_called_with(RoleArn=mock_arn,
-                                                RoleSessionName='temp-session')
+            mock_client.assert_called_with('sts')
+            mock_assume_role.assert_called_with(
+                Policy=json.dumps(aws.cloudigrade_policy),
+                RoleArn=mock_arn,
+                RoleSessionName=f'cloudigrade-{mock_account_id}'
+            )
 
-        self.assertDictEqual(mock_role['Credentials'], actual_credentials)
+        self.assertEquals(creds[0], mock_role['Credentials']['AccessKeyId'])
+        self.assertEquals(
+            creds[1],
+            mock_role['Credentials']['SecretAccessKey']
+        )
+        self.assertEquals(creds[2], mock_role['Credentials']['SessionToken'])
 
     def test_get_running_instances(self):
         """Assert we get expected instances in a dict keyed by regions."""
         mock_arn = helper.generate_dummy_arn()
         mock_regions = [f'region-{uuid.uuid4()}']
-        mock_credentials = {
-            'AccessKeyId': str(uuid.uuid4()),
-            'SecretAccessKey': str(uuid.uuid4()),
-            'SessionToken': str(uuid.uuid4()),
+        mock_role = {
+            'Credentials': {
+                'AccessKeyId': str(uuid.uuid4()),
+                'SecretAccessKey': str(uuid.uuid4()),
+                'SessionToken': str(uuid.uuid4()),
+            },
+            'foo': 'bar',
         }
         mock_running_instance = helper.generate_dummy_describe_instance(
             state=aws.InstanceState.running
@@ -110,61 +124,184 @@ class UtilAwsTest(TestCase):
             mock_regions[0]: [mock_running_instance]
         }
 
-        with patch.object(aws, 'get_credentials_for_arn') as mock_get_creds, \
-                patch.object(aws, 'get_regions') as mock_get_regions, \
+        with patch.object(aws, 'get_regions') as mock_get_regions, \
                 patch.object(aws, 'boto3') as mock_boto3:
-            mock_get_creds.return_value = mock_credentials
+            mock_assume_role = mock_boto3.client.return_value.assume_role
+            mock_assume_role.return_value = mock_role
             mock_get_regions.return_value = mock_regions
             mock_client = mock_boto3.Session.return_value.client.return_value
             mock_client.describe_instances.return_value = mock_described
 
-            actual_found = aws.get_running_instances(mock_arn)
+            actual_found = aws.get_running_instances(aws.get_session(mock_arn))
 
         self.assertDictEqual(expected_found, actual_found)
 
     def test_verify_account_access_success(self):
         """Assert that account access via a IAM role is verified."""
         mock_arn = helper.generate_dummy_arn()
-        mock_credentials = {
-            'AccessKeyId': str(uuid.uuid4()),
-            'SecretAccessKey': str(uuid.uuid4()),
-            'SessionToken': str(uuid.uuid4()),
+        mock_role = {
+            'Credentials': {
+                'AccessKeyId': str(uuid.uuid4()),
+                'SecretAccessKey': str(uuid.uuid4()),
+                'SessionToken': str(uuid.uuid4()),
+            },
+            'foo': 'bar',
         }
+        mock_dry_run_exception = {
+            'Error': {
+                'Code': 'DryRunOperation',
+                'Message': 'Request would have succeeded, '
+                           'but DryRun flag is set.'
+            }
+        }
+        with patch.object(aws, 'boto3') as mock_boto3:
+            mock_assume_role = mock_boto3.client.return_value.assume_role
+            mock_assume_role.return_value = mock_role
 
-        with patch.object(aws, 'get_credentials_for_arn') as mock_get_creds, \
-                patch.object(aws, 'boto3') as __:  # noqa: F841
-            mock_get_creds.return_value = mock_credentials
+            mock_client = mock_boto3.Session.return_value.client.return_value
+            mock_describe_images = mock_client.describe_images
+            mock_describe_instances = mock_client.describe_instances
+            mock_describe_snapshot_attribute = \
+                mock_client.describe_snapshot_attribute
+            mock_describe_snapshots = mock_client.describe_snapshots
+            mock_modify_snapshot_attribute = \
+                mock_client.modify_snapshot_attribute
+            mock_modify_image_attribute = mock_client.modify_image_attribute
 
-            actual_verified = aws.verify_account_access(mock_arn)
+            mock_describe_images.side_effect = ClientError(
+                mock_dry_run_exception, 'DescribeImages')
+            mock_describe_instances.side_effect = ClientError(
+                mock_dry_run_exception, 'DescribeInstances')
+            mock_describe_snapshot_attribute.side_effect = ClientError(
+                mock_dry_run_exception, 'DescribeSnapshotAttribute')
+            mock_describe_snapshots.side_effect = ClientError(
+                mock_dry_run_exception, 'DescribeSnapshots')
+            mock_modify_snapshot_attribute.side_effect = ClientError(
+                mock_dry_run_exception, 'ModifySnapshotAttribute')
+            mock_modify_image_attribute.side_effect = ClientError(
+                mock_dry_run_exception, 'ModifyImageAttribute')
+
+            actual_verified = aws.verify_account_access(
+                aws.get_session(mock_arn))
+
+            mock_describe_images.assert_called_with(DryRun=True)
+            mock_describe_instances.assert_called_with(DryRun=True)
+            mock_describe_snapshot_attribute.assert_called_with(
+                DryRun=True,
+                SnapshotId='string',
+                Attribute='productCodes'
+            )
+            mock_describe_snapshots.assert_called_with(DryRun=True)
+            mock_modify_snapshot_attribute.assert_called_with(
+                SnapshotId='string',
+                DryRun=True,
+                Attribute='productCodes',
+                GroupNames=['string', ]
+            )
+            mock_modify_image_attribute.assert_called_with(
+                Attribute='description',
+                ImageId='string',
+                DryRun=True
+            )
 
         self.assertTrue(actual_verified)
 
     def test_verify_account_access_failure(self):
         """Assert that account access via a IAM role is not verified."""
         mock_arn = helper.generate_dummy_arn()
-        mock_credentials = {
-            'AccessKeyId': str(uuid.uuid4()),
-            'SecretAccessKey': str(uuid.uuid4()),
-            'SessionToken': str(uuid.uuid4()),
+        mock_role = {
+            'Credentials': {
+                'AccessKeyId': str(uuid.uuid4()),
+                'SecretAccessKey': str(uuid.uuid4()),
+                'SessionToken': str(uuid.uuid4()),
+            },
+            'foo': 'bar',
         }
-        errmsg = f'User: {mock_arn} is not authorized to perform: ' + \
-            'ec2:DescribeInstances'
-        error_response = {
+        mock_unauthorized_exception = {
             'Error': {
-                'Message': errmsg,
-                'Code': 'AccessDeniedException'
+                'Code': 'UnauthorizedOperation',
+                'Message': 'You are not authorized '
+                           'to perform this operation.'
             }
         }
-        operation_name = 'DescribeInstances'
+        mock_garbage_exception = {
+            'Error': {
+                'Code': 'GarbageOperation',
+                'Message': 'You are not authorized '
+                           'to perform this garbage operation.'
+            }
+        }
+        bad_policy = {
+            'Version': '2012-10-17',
+            'Statement': [
+                {
+                    'Sid': 'CloudigradeGarbagePolicy',
+                    'Effect': 'Allow',
+                    'Action': [
+                        'ec2:DescribeGarbage'
+                    ],
+                    'Resource': '*'
+                }
+            ]
+        }
 
-        with patch.object(aws, 'get_credentials_for_arn') as mock_get_creds, \
-                patch.object(aws, 'boto3') as mock_boto3:
-            mock_get_creds.return_value = mock_credentials
+        with patch.object(aws, 'boto3') as mock_boto3:
+            mock_assume_role = mock_boto3.client.return_value.assume_role
+            mock_assume_role.return_value = mock_role
+
             mock_client = mock_boto3.Session.return_value.client.return_value
-            mock_client.describe_instances.side_effect = ClientError(
-                error_response,
-                operation_name)
+            mock_describe_images = mock_client.describe_images
+            mock_describe_instances = mock_client.describe_instances
+            mock_describe_snapshot_attribute = \
+                mock_client.describe_snapshot_attribute
+            mock_describe_snapshots = mock_client.describe_snapshots
+            mock_modify_snapshot_attribute = \
+                mock_client.modify_snapshot_attribute
+            mock_modify_image_attribute = mock_client.modify_image_attribute
 
-            actual_verified = aws.verify_account_access(mock_arn)
+            mock_describe_images.side_effect = ClientError(
+                mock_unauthorized_exception, 'DescribeImages')
+            mock_describe_instances.side_effect = ClientError(
+                mock_unauthorized_exception, 'DescribeInstances')
+            mock_describe_snapshot_attribute.side_effect = ClientError(
+                mock_unauthorized_exception, 'DescribeSnapshotAttribute')
+            mock_describe_snapshots.side_effect = ClientError(
+                mock_unauthorized_exception, 'DescribeSnapshots')
+            mock_modify_snapshot_attribute.side_effect = ClientError(
+                mock_unauthorized_exception, 'ModifySnapshotAttribute')
+            mock_modify_image_attribute.side_effect = ClientError(
+                mock_unauthorized_exception, 'ModifyImageAttribute')
+
+            session = aws.get_session(mock_arn)
+            actual_verified = aws.verify_account_access(session)
+
+            mock_describe_images.side_effect = ClientError(
+                mock_garbage_exception, 'DescribeImages')
+            with self.assertRaises(ClientError) as e:
+                    aws.verify_account_access(session)
+                    self.assertContains(e, 'GarbageOperation')
+
+            with patch.dict(aws.cloudigrade_policy, bad_policy):
+                aws.verify_account_access(session)
+
+            mock_describe_images.assert_called_with(DryRun=True)
+            mock_describe_instances.assert_called_with(DryRun=True)
+            mock_describe_snapshot_attribute.assert_called_with(
+                DryRun=True,
+                SnapshotId='string',
+                Attribute='productCodes'
+            )
+            mock_describe_snapshots.assert_called_with(DryRun=True)
+            mock_modify_snapshot_attribute.assert_called_with(
+                SnapshotId='string',
+                DryRun=True,
+                Attribute='productCodes',
+                GroupNames=['string', ]
+            )
+            mock_modify_image_attribute.assert_called_with(
+                Attribute='description',
+                ImageId='string',
+                DryRun=True
+            )
 
         self.assertFalse(actual_verified)
