02c95743c767e91d415896805a2a316c054bbc16
Ilya White <ilya.white@redhat.com>
2018-02-20 19:56:00 -0500

    Update how apps are imported and add tests for */apps.py

---
+++ cloudigrade/util/tests/test_apps.py
@@ -0,0 +1,13 @@
+"""Collection of tests for the App Configuration module."""
+from django.apps import apps
+from django.test import TestCase
+from util.apps import UtilConfig
+
+
+class UtilConfigTest(TestCase):
+    """Add App Configuration Test Case."""
+
+    def test_apps(self):
+        """Test the Util app configuration."""
+        self.assertEqual(UtilConfig.name, 'util')
+        self.assertEqual(apps.get_app_config('util').name, 'util')
