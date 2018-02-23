a1d44a9a8147811a69fdfedd7cc4cd3b51bd14c3
Andrew Berglund <aberglun@redhat.com>
2018-02-23 15:37:57 -0500

    Adding analyzer app to base settings

--- cloudigrade/config/settings/base.py
+++ cloudigrade/config/settings/base.py
@@ -44,6 +44,7 @@ THIRD_PARTY_APPS = []
 LOCAL_APPS = [
     'util.apps.UtilConfig',
     'account.apps.AccountConfig',
+    'analyzer.apps.AnalyzerConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
