6b5abe084d1279fb561127646fddae87a9909737
Andrew Berglund <aberglun@redhat.com>
2018-02-26 13:45:27 -0500

    Cleaned up analyze_log.py.

--- cloudigrade/analyzer/management/commands/analyze_log.py
+++ cloudigrade/analyzer/management/commands/analyze_log.py
@@ -9,25 +9,22 @@ from util import aws
 
 
 class Command(BaseCommand):
-    """Command to store an Amazon ARN in the database.
-
-    This command has the side-effect of also talking to Amazon to fetch and
-    display a list of currently running EC2 instances visible to the ARN.
+    """Command to analyze EC2 events from AWS CloudTrail logs.
 
     Example usage:
 
-        ARN="arn:aws:iam::518028203513:role/grant_cloudi_to_372779871274"
-        python manage.py add_account $ARN
+        QUEUE_URL="https://sqs.us-east-1.amazonaws.com/372779871274/doppler_customer_1_test_bucket_cloudtrail_events"
+        python manage.py analyze_log $QUEUE_URL
     """
 
-    help = _('Stores AWS IAM Role and displays list of its running instances')
+    help = _('Analyzed AWS CloudTrail logs for EC2 on/off events.')
 
     def add_arguments(self, parser):
         """Require an AWS SQS queue URL."""
         parser.add_argument('queue_url', help=_('AWS SQS Queue URL'))
 
     def handle(self, *args, **options):
-        """Extract the account id from the ARN and save both to database."""
+        """Read SQS Queue for log location, and parse log for events."""
         queue_url = options['queue_url']
 
         logs = []
@@ -46,9 +43,14 @@ class Command(BaseCommand):
             key = parsed_message[1]
             logs.append(aws.get_object_content_from_s3(bucket, key))
 
-        # Parse logs for On/Off events
+        # Parse logs for on/off events
         for log in logs:
-            print(self._parse_log_for_ec2_events(log))
+            if log is None or log == '':
+                continue
+            instances, instance_events = self._parse_log_for_ec2_events(log)
+            result = 'Found instances: {i} and events {e}'.format(
+                i=instances, e=instance_events)
+            self.stdout.write(_(result))
 
         # TODO: Save the messages to the DB
         # TODO: If we haven't seen this instance before we'll
@@ -59,6 +61,16 @@ class Command(BaseCommand):
         # aws.delete_message_from_queue(queue_url, messages)
 
     def _parse_sqs_message(self, message):
+        """
+        Parse SQS message for path to log in S3.
+
+        Args:
+            message (boto3.SQS.Message): The Message object.
+
+        Returns:
+            list(tuple): List of (bucket, key) log file locations in S3.
+
+        """
         parsed_records = []
         message_body = json.loads(message.body)
         for record in message_body.get('Records', []):
@@ -68,6 +80,17 @@ class Command(BaseCommand):
         return parsed_records
 
     def _parse_log_for_ec2_events(self, log):
+        """
+        Parse S3 log for EC2 on/off events.
+
+        Args:
+            log (string): The string contents of the log file.
+
+        Returns:
+            list(tuple): List of instance data seen in log.
+            list(tuple): List of instance_event data seen in log.
+
+        """
         ec2_event_map = {
             'RunInstances': 'power_on',
             'StartInstances': 'power_on',
@@ -77,14 +100,9 @@ class Command(BaseCommand):
         instances = []
         instance_events = []
         log = json.loads(log)
+
         for record in log.get('Records', []):
-            if record.get('eventSource') != 'ec2.amazonaws.com':
-                continue
-            # Currently we do not store events that have an error
-            elif record.get('errorCode'):
-                continue
-            # Currently we only want power on/power off EC2 events
-            elif record.get('eventName') not in ec2_event_map.keys():
+            if not self._is_on_off_event(record, ec2_event_map.keys()):
                 continue
 
             account_id = record.get('userIdentity', {}).get('accountId')
@@ -114,4 +132,27 @@ class Command(BaseCommand):
                      instance_type)
                 )
 
-            return list(set(instances)), instance_events
+        return list(set(instances)), instance_events
+
+    def _is_on_off_event(self, record, valid_events):
+        """
+        Determine if a log event is an EC2 on/off event.
+
+        Args:
+            record (dict): The log record record.
+            valid_events (list): Events considered to be on/off.
+
+        Returns:
+            bool: Whether the record contains an on/off event
+
+        """
+        if record.get('eventSource') != 'ec2.amazonaws.com':
+            return False
+        # Currently we do not store events that have an error
+        elif record.get('errorCode'):
+            return False
+        # Currently we only want power on/power off EC2 events
+        elif record.get('eventName') not in valid_events:
+            return False
+        else:
+            return True
