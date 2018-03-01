149c647e3c39f6e88d99acc787d7efa1f91b772a
Andrew Berglund <aberglun@redhat.com>
2018-03-01 13:10:53 -0500

    Updated analyze_log tests for DB saving step.

--- cloudigrade/analyzer/tests/test_management_commands.py
+++ cloudigrade/analyzer/tests/test_management_commands.py
@@ -5,39 +5,46 @@ import uuid
 from io import StringIO
 from unittest.mock import patch
 
+from dateutil import tz
 from django.core.management import call_command
 from django.test import TestCase
 from django.utils.translation import gettext as _
 
-from account.management.commands.add_account import aws
-from util.tests import helper
+from account.models import Instance, InstanceEvent
+from account.tests import helper as account_helper
+from analyzer.management.commands.analyze_log import aws
+from util.tests import helper as util_helper
 
 
 class AnalyzeLogTest(TestCase):
     """Analyze Log management command test case."""
 
-    def test_command_output_success(self):
+    def setUp(self):
+        """Set up common variables for tests."""
+        self.mock_account_id = str(util_helper.generate_dummy_aws_account_id())
+        self.mock_arn = util_helper.generate_dummy_arn(self.mock_account_id)
+        self.mock_account = account_helper.generate_account(
+            self.mock_arn, self.mock_account_id,
+        )
+
+    def test_command_output_success_ec2_attributes_included(self):
         """Test processing a CloudTrail log."""
         out = StringIO()
         mock_queue_url = 'https://sqs.queue.url'
         mock_receipt_handle = str(uuid.uuid4())
-        mock_account_id = str(helper.generate_dummy_aws_account_id())
         mock_instance_id = str(uuid.uuid4())
         mock_region = 'us-east-1'
         mock_event_type = 'RunInstances'
-        mock_occured_at = datetime.datetime.utcnow().isoformat()
+        now = datetime.datetime.utcnow()
+        # Work around for sqlite handling of microseconds
+        mock_occurred_at = datetime.datetime(
+            year=now.year, month=now.month, day=now.day, hour=now.hour,
+            minute=now.minute, second=now.second, tzinfo=tz.tzutc()
+        )
         mock_subnet = 'subnet-9000'
         mock_ec2_ami_id = str(uuid.uuid4())
         mock_instance_type = 't2.nano'
 
-        expected_instances = [(mock_account_id, mock_instance_id, mock_region)]
-        expected_instance_events = [
-            (mock_instance_id, 'power_on', mock_occured_at, mock_subnet,
-             mock_ec2_ami_id, mock_instance_type)
-        ]
-
-        expected_out = _('Found instances: {i} and events {e}').format(
-            i=expected_instances, e=expected_instance_events)
         mock_sqs_message_body = {
             'Records': [
                 {
@@ -52,7 +59,7 @@ class AnalyzeLogTest(TestCase):
                 }
             ]
         }
-        mock_message = helper.generate_mock_sqs_message(
+        mock_message = util_helper.generate_mock_sqs_message(
             mock_queue_url,
             json.dumps(mock_sqs_message_body),
             mock_receipt_handle
@@ -64,7 +71,9 @@ class AnalyzeLogTest(TestCase):
                     'awsRegion': mock_region,
                     'eventName': mock_event_type,
                     'eventSource': 'ec2.amazonaws.com',
-                    'eventTime': mock_occured_at,
+                    'eventTime': mock_occurred_at.strftime(
+                        '%Y-%m-%dT%H:%M:%SZ'
+                    ),
                     'eventType': 'AwsApiCall',
                     'responseElements': {
                         'instancesSet': {
@@ -79,27 +88,158 @@ class AnalyzeLogTest(TestCase):
                         },
                     },
                     'userIdentity': {
-                        'accountId': mock_account_id
+                        'accountId': self.mock_account_id
                     }
                 }
             ]
         }
 
+        expected_out = _('Saved instances and/or events to the DB.')
+
         with patch.object(aws, 'receive_message_from_queue') as mock_receive, \
                 patch.object(aws, 'get_object_content_from_s3') as mock_s3, \
-                patch.object(aws, 'delete_message_from_queue') as mock_delete:
+                patch.object(aws, 'delete_message_from_queue') as mock_del:
             mock_receive.return_value = [mock_message]
             mock_s3.return_value = json.dumps(mock_cloudtrail_log)
-            mock_delete.return_value = 'Success'
+            mock_del.return_value = 'Success'
 
             call_command('analyze_log', mock_queue_url, stdout=out)
 
-        actual_stdout = out.getvalue()
-        self.assertIn(expected_out, actual_stdout)
+        actual_stdout = out.getvalue().strip()
+
+        self.assertIn(actual_stdout, expected_out)
+
+        instances = list(Instance.objects.filter(
+            ec2_instance_id=mock_instance_id).all())
+        instance_events = list(InstanceEvent.objects.filter(
+            instance=instances[0]).all()) if instances else []
+
+        for instance in instances:
+            self.assertEqual(instance.account, self.mock_account)
+            self.assertEqual(instance.ec2_instance_id, mock_instance_id)
+            self.assertEqual(instance.region, mock_region)
+
+        for event in instance_events:
+            self.assertEqual(event.instance, instances[0])
+            self.assertEqual(event.event_type, InstanceEvent.TYPE.power_on)
+            self.assertEqual(event.occurred_at, mock_occurred_at)
+            self.assertEqual(event.subnet, mock_subnet)
+            self.assertEqual(event.ec2_ami_id, mock_ec2_ami_id)
+            self.assertEqual(event.instance_type, mock_instance_type)
+
+    def test_command_output_success_lookup_ec2_attributes(self):
+        """Test processing a CloudTrail log."""
+        out = StringIO()
+        mock_queue_url = 'https://sqs.queue.url'
+        mock_receipt_handle = str(uuid.uuid4())
+        mock_instance_id = str(uuid.uuid4())
+        mock_region = 'us-east-1'
+        mock_event_type = 'StartInstances'
+        now = datetime.datetime.utcnow()
+        # Work around for sqlite handling of microseconds
+        mock_occurred_at = datetime.datetime(
+            year=now.year, month=now.month, day=now.day, hour=now.hour,
+            minute=now.minute, second=now.second, tzinfo=tz.tzutc()
+        )
+        mock_subnet = 'subnet-9000'
+        mock_ec2_ami_id = str(uuid.uuid4())
+        mock_instance_type = 't2.nano'
+
+        mock_instance = util_helper.generate_mock_ec2_instance(
+            mock_instance_id, mock_ec2_ami_id, mock_subnet, None,
+            mock_instance_type
+        )
+
+        mock_sqs_message_body = {
+            'Records': [
+                {
+                    's3': {
+                        'bucket': {
+                            'name': 'test-bucket',
+                        },
+                        'object': {
+                            'key': 'path/to/log/log.json.gz',
+                        },
+                    },
+                }
+            ]
+        }
+        mock_message = util_helper.generate_mock_sqs_message(
+            mock_queue_url,
+            json.dumps(mock_sqs_message_body),
+            mock_receipt_handle
+        )
+
+        mock_cloudtrail_log = {
+            'Records': [
+                {
+                    'awsRegion': mock_region,
+                    'eventName': mock_event_type,
+                    'eventSource': 'ec2.amazonaws.com',
+                    'eventTime': mock_occurred_at.strftime(
+                        '%Y-%m-%dT%H:%M:%SZ'
+                    ),
+                    'eventType': 'AwsApiCall',
+                    'responseElements': {
+                        'instancesSet': {
+                            'items': [
+                                {
+                                    'imageId': None,
+                                    'instanceId': mock_instance_id,
+                                    'instanceType': None,
+                                    'subnetId': None
+                                }
+                            ]
+                        },
+                    },
+                    'userIdentity': {
+                        'accountId': self.mock_account_id
+                    }
+                }
+            ]
+        }
+
+        expected_out = _('Saved instances and/or events to the DB.')
+
+        with patch.object(aws, 'receive_message_from_queue') as mock_receive, \
+                patch.object(aws, 'get_object_content_from_s3') as mock_s3, \
+                patch.object(aws, 'delete_message_from_queue') as mock_del, \
+                patch.object(aws, 'get_session') as mock_session,\
+                patch.object(aws, 'get_ec2_instance') as mock_ec2:
+            mock_receive.return_value = [mock_message]
+            mock_s3.return_value = json.dumps(mock_cloudtrail_log)
+            mock_del.return_value = 'Success'
+            mock_session.return_value = 'Session'
+            mock_ec2.return_value = mock_instance
+
+            call_command('analyze_log', mock_queue_url, stdout=out)
+
+        actual_stdout = out.getvalue().strip()
+
+        self.assertIn(actual_stdout, expected_out)
+
+        instances = list(Instance.objects.filter(
+            ec2_instance_id=mock_instance_id).all())
+        instance_events = list(InstanceEvent.objects.filter(
+            instance=instances[0]).all()) if instances else []
+
+        for instance in instances:
+            self.assertEqual(instance.account, self.mock_account)
+            self.assertEqual(instance.ec2_instance_id, mock_instance_id)
+            self.assertEqual(instance.region, mock_region)
+
+        for event in instance_events:
+            self.assertEqual(event.instance, instances[0])
+            self.assertEqual(event.event_type, InstanceEvent.TYPE.power_on)
+            self.assertEqual(event.occurred_at, mock_occurred_at)
+            self.assertEqual(event.subnet, mock_subnet)
+            self.assertEqual(event.ec2_ami_id, mock_ec2_ami_id)
+            self.assertEqual(event.instance_type, mock_instance_type)
 
     def test_command_output_no_log_content(self):
         """Test that a non-log is not processed."""
         out = StringIO()
+        mock_instance_id = str(uuid.uuid4())
         mock_queue_url = 'https://sqs.queue.url'
         mock_receipt_handle = str(uuid.uuid4())
 
@@ -117,7 +257,7 @@ class AnalyzeLogTest(TestCase):
                 }
             ]
         }
-        mock_message = helper.generate_mock_sqs_message(
+        mock_message = util_helper.generate_mock_sqs_message(
             mock_queue_url,
             json.dumps(mock_sqs_message_body),
             mock_receipt_handle
@@ -125,21 +265,32 @@ class AnalyzeLogTest(TestCase):
 
         mock_cloudtrail_log = ''
 
+        expected_out = _('No instances or events to save to the DB.')
+
         with patch.object(aws, 'receive_message_from_queue') as mock_receive, \
                 patch.object(aws, 'get_object_content_from_s3') as mock_s3, \
-                patch.object(aws, 'delete_message_from_queue') as mock_delete:
+                patch.object(aws, 'delete_message_from_queue') as mock_del:
             mock_receive.return_value = [mock_message]
             mock_s3.return_value = mock_cloudtrail_log
-            mock_delete.return_value = 'Success'
+            mock_del.return_value = 'Success'
 
             call_command('analyze_log', mock_queue_url, stdout=out)
 
         actual_stdout = out.getvalue()
-        self.assertIn('', actual_stdout)
+        self.assertIn(expected_out, actual_stdout)
+
+        instances = list(Instance.objects.filter(
+            ec2_instance_id=mock_instance_id).all())
+        instance_events = list(InstanceEvent.objects.filter(
+            instance=instances[0]).all()) if instances else []
+
+        self.assertListEqual(instances, [])
+        self.assertListEqual(instance_events, [])
 
     def test_command_output_non_on_off_events(self):
         """Test that non on/off events are not processed."""
         out = StringIO()
+        mock_instance_id = str(uuid.uuid4())
         mock_queue_url = 'https://sqs.queue.url'
         mock_receipt_handle = str(uuid.uuid4())
 
@@ -157,7 +308,7 @@ class AnalyzeLogTest(TestCase):
                 }
             ]
         }
-        mock_message = helper.generate_mock_sqs_message(
+        mock_message = util_helper.generate_mock_sqs_message(
             mock_queue_url,
             json.dumps(mock_sqs_message_body),
             mock_receipt_handle
@@ -177,6 +328,8 @@ class AnalyzeLogTest(TestCase):
             ]
         }
 
+        expected_out = _('No instances or events to save to the DB.')
+
         with patch.object(aws, 'receive_message_from_queue') as mock_receive, \
                 patch.object(aws, 'get_object_content_from_s3') as mock_s3, \
                 patch.object(aws, 'delete_message_from_queue') as mock_delete:
@@ -187,4 +340,12 @@ class AnalyzeLogTest(TestCase):
             call_command('analyze_log', mock_queue_url, stdout=out)
 
         actual_stdout = out.getvalue()
-        self.assertIn('', actual_stdout)
+        self.assertIn(expected_out, actual_stdout)
+
+        instances = list(Instance.objects.filter(
+            ec2_instance_id=mock_instance_id).all())
+        instance_events = list(InstanceEvent.objects.filter(
+            instance=instances[0]).all()) if instances else []
+
+        self.assertListEqual(instances, [])
+        self.assertListEqual(instance_events, [])
