54d74b355c4f8ef9e37edbcdda91607348942567
Brad Smith <brasmith@redhat.com>
2018-03-05 10:50:53 -0500

    split drf serializers into separate module

--- cloudigrade/account/views.py
+++ cloudigrade/account/views.py
@@ -1,19 +1,12 @@
-"""DRF API serializers for the account app."""
-from rest_framework import serializers, viewsets
+"""DRF API views for the account app."""
+from rest_framework import viewsets
 
+from account import serializers
 from account.models import Account
-
-
-class AccountSerializer(serializers.HyperlinkedModelSerializer):
-    """Serialize a customer Account for the API."""
-
-    class Meta:
-        model = Account
-        fields = ('id', 'url', 'account_id', 'account_arn')
 
 
 class AccountViewSet(viewsets.ReadOnlyModelViewSet):
     """List all or retrieve a single customer Account."""
 
     queryset = Account.objects.all()
-    serializer_class = AccountSerializer
+    serializer_class = serializers.AccountSerializer
