98c2dc92b0eacda44585ff831e5764841e591705
Andrew Berglund <aberglun@redhat.com>
2018-03-20 12:56:40 -0400

    Added utility functions to save machine images and process messages. * Save new AMIs to DB * Process instance/AMI data into message format * Send message to RabbitMQ message queue

--- cloudigrade/account/util.py
+++ cloudigrade/account/util.py
@@ -1,9 +1,13 @@
 """Various utility functions for the account app."""
 import collections
+import json
 
+import pika
+from django.conf import settings
 from django.utils import timezone
 
-from account.models import AwsInstance, AwsInstanceEvent, InstanceEvent
+from account.models import (AwsInstance, AwsInstanceEvent, InstanceEvent,
+                            MachineImage, AWSMachineImage)
 
 
 def create_initial_aws_instance_events(account, instances_data):
@@ -34,12 +38,15 @@ def create_initial_aws_instance_events(account, instances_data):
                 ec2_instance_id=instance_data['InstanceId'],
                 region=region,
             )
+            aws_machine_image = AWSMachineImage.objects.get(
+                ec2_ami_id=instance_data['ImageId']
+            )
             event = AwsInstanceEvent(
                 instance=instance,
                 event_type=InstanceEvent.TYPE.power_on,
                 occurred_at=timezone.now(),
                 subnet=instance_data['SubnetId'],
-                ec2_ami_id=instance_data['ImageId'],
+                ec2_ami_id=aws_machine_image,
                 instance_type=instance_data['InstanceType'],
             )
             event.save()
@@ -48,19 +55,53 @@ def create_initial_aws_instance_events(account, instances_data):
 
 
 def create_new_machine_images(instances_data):
-    platforms = {instance['ImageId']: instance['Platform']
-                 for instance in instance_data
-                 if instance['Platform'] == 'Windows'}
-    seen_amis = set([instance['ImageId'] for instance in instance_data])
-    known_amis = MachineImage.objects.filter(ami_id__in=list(amis))
-    new_amis = list(seen_amis.difference(known_amis))
+    saved_amis = []
+    for __, instances in instances_data.items():
+        print(instances)
 
-    for new_ami in new_amis:
-        ami = MachineImage(
-            ami_id = new_ami,
-            platform = MachineImage.TYPE.Windows if platforms.get(new_ami) \
-                else MachineImage.TYPE.Linux
+        platforms = {instance['ImageId']: instance['Platform']
+                     for instance in instances
+                     if instance.get('Platform') == 'Windows'}
+        seen_amis = set([instance['ImageId'] for instance in instances])
+        known_amis = AWSMachineImage.objects.filter(
+            ec2_ami_id__in=list(seen_amis)
         )
-        ami.save()
+        new_amis = list(seen_amis.difference(known_amis))
 
-    return new_amis
+        for new_ami in new_amis:
+            ami = AWSMachineImage(
+                ec2_ami_id=new_ami,
+                platform=MachineImage.TYPE.Windows if platforms.get(new_ami) \
+                    else MachineImage.TYPE.Linux
+            )
+            ami.save()
+
+        saved_amis.extend(new_amis)
+    return saved_amis
+
+
+def generate_aws_ami_messages(instances_data, ami_list):
+    messages = []
+    for region, instances in instances_data.items():
+        for instance in instances:
+            if instance['ImageId'] in ami_list:
+                messages.append(json.dumps({"cloud_provider": 'AWS',
+                                            "region": region,
+                                            "image_id": instance['ImageId']}))
+    return messages
+
+
+def add_messages_to_queue(queue_name, messages):
+    connection = pika.BlockingConnection(
+        pika.URLParameters(settings.CELERY_BROKER_URL)
+    )
+
+    channel = connection.channel()
+    channel.queue_declare(queue=queue_name)
+
+    for message in messages:
+        channel.basic_publish(exchange='',
+                              routing_key=queue_name,
+                              body=message)
+
+    connection.close()
