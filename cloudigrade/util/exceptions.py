e954435b3f950681bcc4b6afdd547219fb00664d
Andrew Berglund <aberglun@redhat.com>
2018-04-09 13:29:20 -0400

    Added exceptions around snapshot copying

--- cloudigrade/util/exceptions.py
+++ cloudigrade/util/exceptions.py
@@ -6,3 +6,27 @@ class InvalidArn(ParseError):
     """an invalid ARN was detected."""
 
     pass
+
+
+class AwsValueError(ValueError):
+    """Inapropriate value given to function."""
+
+    pass
+
+
+class AwsSnapshotCopyLimitError(Exception):
+    """The limit on concurrent snapshot copy operations has been met."""
+
+    pass
+
+
+class AwsSnapshotEncryptedError(Exception):
+    """Raise when a snapshot is encrypted."""
+
+    pass
+
+
+class AwsSnapshotNotOwnedError(Exception):
+    """Raise when our account id does not have permissions on the snapshot."""
+
+    pass
