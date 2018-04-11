e0232ff309a94986730eb2416996f9911b054276
Ilya White <ilya.white@redhat.com>
2018-04-11 14:54:41 -0400

    Update prod settings and add .s2iignore

--- cloudigrade/config/settings/prod.py
+++ cloudigrade/config/settings/prod.py
@@ -1,5 +1,18 @@
 from .base import *  # noqa
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
-DATABASES['default'] = env.db()
+ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS')
+DATABASES = {
+    'default': {
+        'ENGINE': env('DJANGO_DATABASE_ENGINE', default=django.db.backends.postgresql_psycopg2),
+        'NAME': env('DJANGO_DATABASE_NAME'),
+        'HOST': env('DJANGO_DATABASE_HOST'),
+        'USER': env('DJANGO_DATABASE_USER'),
+        'PASSWORD': env('DJANGO_DATABASE_PASSWORD'),
+        'PORT': env('DJANGO_DATABASE_PORT', default=5432),
+        'CONN_MAX_AGE': env('DJANGO_DATABASE_CONN_MAX_AGE', default=0),
+    }
+}
+
+RABBITMQ_URL = env('DJANGO_RABBITMQ_URL')
+STATIC_ROOT = env('DJANGO_STATIC_ROOT', default='/srv/cloudigrade/static/')
