78cb2a650d15ae91f6419c0604da0e765c56a5aa
Ilya White <ilya.white@redhat.com>
2018-04-11 16:58:21 -0400

    Make our settings more consistent

--- cloudigrade/config/settings/docker.py
+++ cloudigrade/config/settings/docker.py
@@ -2,7 +2,7 @@ from .base import *  # noqa
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='docker')
-ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS', default=['*'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['*'])
 
 DATABASES = {
     'default': env.db(default='postgres://postgres:postgres@db:5432/postgres')
@@ -11,6 +11,6 @@ DATABASES = {
 # Message and Task Queues
 
 RABBITMQ_URL = env('RABBITMQ_URL', default='amqp://guest:guest@localhost:5672/%2F')
-CELERY_BROKER_URL = env('RABBITMQ_URL', default='amqp://guest:guest@localhost:5672/%2F')
+CELERY_BROKER_URL = RABBITMQ_URL
 
-STATIC_ROOT = env('STATIC_ROOT', default='/srv/cloudigrade/static/')
+STATIC_ROOT = env('DJANGO_STATIC_ROOT', default='/srv/cloudigrade/static/')
