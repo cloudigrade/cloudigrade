5a76119a1c70d348277e56a8d01d17ebdcd105e2
Ilya White <ilya.white@redhat.com>
2018-04-24 15:32:21 -0400

    Add support for local development against the OpenShift cluster

--- cloudigrade/config/settings/local.py
+++ cloudigrade/config/settings/local.py
@@ -4,4 +4,3 @@ from .base import *
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='local')
 
-DATABASES['default']['PORT'] = env('DJANGO_DATABASE_PORT', default='15432')
