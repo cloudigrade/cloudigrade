ecdd563d63f2fa17805cbb6037cb3eaec747ab27
Brad Smith <brasmith@redhat.com>
2018-04-25 17:02:39 -0400

    make import style more consistent with the rest of the project

--- cloudigrade/util/aws/__init__.py
+++ cloudigrade/util/aws/__init__.py
@@ -1,12 +1,14 @@
 """Helper utility package to wrap up common AWS operations."""
 from util.aws.arn import AwsArn
-from util.aws.ec2 import InstanceState, add_snapshot_ownership, \
-    check_volume_state, copy_snapshot, create_volume, get_ami, \
-    get_ami_snapshot_id, get_ec2_instance, get_running_instances, get_snapshot, \
-    get_volume
-from util.aws.helper import get_regions, rewrap_aws_errors, \
-    verify_account_access, get_region_from_availability_zone
+from util.aws.ec2 import (InstanceState, add_snapshot_ownership,
+                          check_volume_state, copy_snapshot, create_volume,
+                          get_ami,
+                          get_ami_snapshot_id, get_ec2_instance,
+                          get_running_instances, get_snapshot,
+                          get_volume)
+from util.aws.helper import (get_region_from_availability_zone, get_regions,
+                             rewrap_aws_errors, verify_account_access)
 from util.aws.s3 import get_object_content_from_s3
-from util.aws.sqs import delete_message_from_queue, extract_sqs_message, \
-    receive_message_from_queue
+from util.aws.sqs import (delete_message_from_queue, extract_sqs_message,
+                          receive_message_from_queue)
 from util.aws.sts import get_session
