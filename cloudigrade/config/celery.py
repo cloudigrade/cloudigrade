7a7e0e6c2888df6ca0b1e93044fbe8f4e456f6bc
Brad Smith <brasmith@redhat.com>
2018-05-01 14:24:21 -0400

    add deployment config for cloudigrade-celery

--- cloudigrade/config/celery.py
+++ cloudigrade/config/celery.py
@@ -1,5 +1,9 @@
 """Celery app for use in Django project."""
+import django
 from celery import Celery
+
+# Django setup is required *before* Celery app can start correctly.
+django.setup()
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
