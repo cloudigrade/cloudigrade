cd9eb929a70f8b35f2b94f8078633fd369965b17
Brad Smith <brasmith@redhat.com>
2018-05-04 14:54:59 -0400

    fix to get instances from all reservations, not just the last one

--- cloudigrade/util/tests/test_aws_ec2.py
+++ cloudigrade/util/tests/test_aws_ec2.py
@@ -18,7 +18,16 @@ class UtilAwsEc2Test(TestCase):
     """AWS EC2 utility functions test case."""
 
     def test_get_running_instances(self):
-        """Assert we get expected instances in a dict keyed by regions."""
+        """
+        Assert we get expected instances in a dict keyed by regions.
+
+        The setup here is a little complicated, and it's important to
+        understand what's going into it. The mock response from the client's
+        `describe_instances` includes a Reservations list of **three** elements
+        with different Instances in each. It is unclear to us how AWS divides
+        Instances into Reservations; so, we must ensure in our tests that we
+        are checking for Instances in potentially multiple Reservations.
+        """
         mock_regions = [f'region-{uuid.uuid4()}']
         mock_role = helper.generate_dummy_role()
 
@@ -26,18 +35,34 @@ class UtilAwsEc2Test(TestCase):
         mock_assume_role = mock_session.client.return_value.assume_role
         mock_assume_role.return_value = mock_role
 
-        mock_running_instance = helper.generate_dummy_describe_instance(
+        mock_running_instance_1 = helper.generate_dummy_describe_instance(
             state=ec2.InstanceState.running
         )
-        mock_stopped_instance = helper.generate_dummy_describe_instance(
+        mock_running_instance_2 = helper.generate_dummy_describe_instance(
+            state=ec2.InstanceState.running
+        )
+        mock_stopped_instance_1 = helper.generate_dummy_describe_instance(
+            state=ec2.InstanceState.stopped
+        )
+        mock_stopped_instance_2 = helper.generate_dummy_describe_instance(
             state=ec2.InstanceState.stopped
         )
         mock_described = {
             'Reservations': [
                 {
                     'Instances': [
-                        mock_running_instance,
-                        mock_stopped_instance,
+                        mock_running_instance_1,
+                        mock_stopped_instance_1,
+                    ],
+                },
+                {
+                    'Instances': [
+                        mock_running_instance_2,
+                    ],
+                },
+                {
+                    'Instances': [
+                        mock_stopped_instance_2,
                     ],
                 },
             ],
@@ -47,7 +72,10 @@ class UtilAwsEc2Test(TestCase):
         mock_client.describe_instances.return_value = mock_described
 
         expected_found = {
-            mock_regions[0]: [mock_running_instance]
+            mock_regions[0]: [
+                mock_running_instance_1,
+                mock_running_instance_2,
+            ]
         }
 
         with patch.object(ec2, 'get_regions') as mock_get_regions:
