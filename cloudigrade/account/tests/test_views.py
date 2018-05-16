b3d386b0507dd93bc496fa9a2cb4a85eddc559f1
Kevan Holdaway <32331547+kholdaway@users.noreply.github.com>
2018-05-16 18:09:51 -0400

    List/retrieve machine images via REST (#293)

--- cloudigrade/account/tests/test_views.py
+++ cloudigrade/account/tests/test_views.py
@@ -7,9 +7,12 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 
 from account import AWS_PROVIDER_STRING
 from account.exceptions import InvalidCloudProviderError
-from account.models import AwsAccount, AwsInstance
+from account.models import AwsAccount, AwsInstance, AwsMachineImage
 from account.tests import helper as account_helper
-from account.views import AccountViewSet, InstanceViewSet, ReportViewSet
+from account.views import (AccountViewSet,
+                           InstanceViewSet,
+                           MachineImageViewSet,
+                           ReportViewSet)
 from util.tests import helper as util_helper
 
 
@@ -395,4 +398,180 @@ class InstanceViewSetTest(TestCase):
         """Assert that the list instances returns 400 with bad user_id."""
         params = {'user_id': 'not_an_int'}
         response = self.get_instance_list_response(self.superuser, params)
+        self.assertEqual(response.status_code, 400)
+
+
+class MachineImageViewSetTest(TestCase):
+    """MachineImageViewSet test case."""
+
+    def setUp(self):
+        """Set up a bunch of test data."""
+        self.user1 = util_helper.generate_test_user()
+        self.user2 = util_helper.generate_test_user()
+        self.superuser = util_helper.generate_test_user(is_superuser=True)
+        self.account1 = account_helper.generate_aws_account(user=self.user1)
+        self.account2 = account_helper.generate_aws_account(user=self.user1)
+        self.account3 = account_helper.generate_aws_account(user=self.user2)
+        self.account4 = account_helper.generate_aws_account(user=self.user2)
+        self.machine_image1 = \
+            account_helper.generate_aws_image(account=self.account1)
+        self.machine_image2 = \
+            account_helper.generate_aws_image(account=self.account2)
+        self.machine_image3 = \
+            account_helper.generate_aws_image(account=self.account3)
+        self.machine_image4 = \
+            account_helper.generate_aws_image(account=self.account4)
+        self.factory = APIRequestFactory()
+
+    def assertResponseHasImageData(self, response, image):
+        """Assert the response has data matching the image object."""
+        self.assertEqual(
+            response.data['id'], image.id
+        )
+        self.assertEqual(
+            response.data['account'],
+            f'http://testserver/api/v1/account/{image.account.id}/'
+        )
+        self.assertEqual(
+            response.data['resourcetype'], image.__class__.__name__
+        )
+
+        if isinstance(image, AwsMachineImage):
+            self.assertEqual(
+                response.data['ec2_ami_id'], image.ec2_ami_id
+            )
+            self.assertEqual(
+                response.data['is_windows'], image.is_windows
+            )
+            self.assertEqual(
+                response.data['is_encrypted'], image.is_encrypted
+            )
+
+    def get_image_get_response(self, user, image_id):
+        """
+        Generate a response for a get-retrieve on the MachineImageViewSet.
+
+        Args:
+            user (User): Django auth user performing the request
+            image_id (int): the id of the image to retrieve
+            data (dict): optional data to use as query params
+
+        Returns:
+            Response: the generated response for this request
+
+        """
+        request = self.factory.get('/image/')
+        force_authenticate(request, user=user)
+        view = MachineImageViewSet.as_view(actions={'get': 'retrieve'})
+        response = view(request, pk=image_id)
+        return response
+
+    def get_image_list_response(self, user, data=None):
+        """
+        Generate a response for a get-list on the MachineImageViewSet.
+
+        Args:
+            user (User): Django auth user performing the request
+            data (dict): optional data to use as query params
+
+        Returns:
+            Response: the generated response for this request
+
+        """
+        request = self.factory.get('/image/', data)
+        force_authenticate(request, user=user)
+        view = MachineImageViewSet.as_view(actions={'get': 'list'})
+        response = view(request)
+        return response
+
+    def get_image_ids_from_list_response(self, response):
+        """
+        Get the image id values from the paginated response.
+
+        Args:
+            response (Response): Django response object to inspect
+
+        Returns:
+            set[int]: the image id values found in the response
+
+        """
+        image_ids = set([
+            image['id'] for image in response.data['results']
+        ])
+        return image_ids
+
+    def test_list_images_as_user1(self):
+        """Assert that user1 sees only its own images."""
+        expected_images = {
+            self.machine_image1.id,
+            self.machine_image2.id,
+        }
+        response = self.get_image_list_response(self.user1)
+        actual_images = self.get_image_ids_from_list_response(response)
+        self.assertEqual(expected_images, actual_images)
+
+    def test_list_images_as_user2(self):
+        """Assert that user2 sees only its own images."""
+        expected_images = {
+            self.machine_image3.id,
+            self.machine_image4.id,
+        }
+        response = self.get_image_list_response(self.user2)
+        actual_images = self.get_image_ids_from_list_response(response)
+        self.assertEqual(expected_images, actual_images)
+
+    def test_list_images_as_superuser(self):
+        """Assert that the superuser sees all images regardless of owner."""
+        expected_images = {
+            self.machine_image1.id,
+            self.machine_image2.id,
+            self.machine_image3.id,
+            self.machine_image4.id
+        }
+        response = self.get_image_list_response(self.superuser)
+        actual_images = self.get_image_ids_from_list_response(response)
+        self.assertEqual(expected_images, actual_images)
+
+    def test_list_images_as_superuser_with_filter(self):
+        """Assert that the superuser sees images filtered by user_id."""
+        expected_images = {
+            self.machine_image3.id,
+            self.machine_image4.id
+        }
+        params = {'user_id': self.user2.id}
+        response = self.get_image_list_response(self.superuser, params)
+        actual_images = self.get_image_ids_from_list_response(response)
+        self.assertEqual(expected_images, actual_images)
+
+    def test_get_user1s_image_as_user1_returns_ok(self):
+        """Assert that user1 can get one of its own images."""
+        user = self.user1
+        image = self.machine_image2  # Image belongs to user1.
+
+        response = self.get_image_get_response(user, image.id)
+        self.assertEqual(response.status_code, 200)
+        self.assertResponseHasImageData(response, image)
+
+    def test_get_user1s_image_as_user2_returns_404(self):
+        """Assert that user2 cannot get an image belonging to user1."""
+        user = self.user2
+        image = self.machine_image2  # Image belongs to user1, NOT user2.
+
+        response = self.get_image_get_response(user, image.id)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_user1s_image_as_superuser_returns_ok(self):
+        """Assert that superuser can get another user's images."""
+        user = self.superuser
+        # Image belongs to user1, NOT superuser.
+        image = self.machine_image2
+
+        response = self.get_image_get_response(user, image.id)
+        self.assertEqual(response.status_code, 200)
+        self.assertResponseHasImageData(response, image)
+
+    def test_list_images_as_superuser_with_bad_filter(self):
+        """Assert that the list images returns 400 with bad user_id."""
+        params = {'user_id': 'not_an_int'}
+        response = self.get_image_list_response(self.superuser, params)
         self.assertEqual(response.status_code, 400)
