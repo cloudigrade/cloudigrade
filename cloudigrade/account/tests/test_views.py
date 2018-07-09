ea873deee25a7b09d66fdc73f568d5f62407333a
Ashley Aiken <34656858+abaiken@users.noreply.github.com>
2018-07-09 16:17:39 -0400

    API for totals per clount (cloud account overview) (#403)

--- cloudigrade/account/tests/test_views.py
+++ cloudigrade/account/tests/test_views.py
@@ -15,9 +15,11 @@ from account.models import (AwsAccount,
                             AwsInstance,
                             AwsInstanceEvent,
                             AwsMachineImage,
-                            ImageTag)
+                            ImageTag,
+                            InstanceEvent)
 from account.tests import helper as account_helper
 from account.views import (AccountViewSet,
+                           CloudAccountOverviewViewSet,
                            InstanceEventViewSet,
                            InstanceViewSet,
                            MachineImageViewSet,
@@ -950,3 +952,212 @@ class SysconfigViewSetTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {'aws_account_id': account_id})
+
+
+class CloudAccountOverviewViewSetTest(TestCase):
+    """CloudAccountOverviewViewSet test case."""
+
+    def setUp(self):
+        """Set up a bunch of test data."""
+        self.user1 = util_helper.generate_test_user()
+        self.user2 = util_helper.generate_test_user()
+        self.start = util_helper.utc_dt(2018, 1, 1, 0, 0, 0)
+        self.end = util_helper.utc_dt(2018, 2, 1, 0, 0, 0)
+        powered_time = util_helper.utc_dt(2018, 1, 10, 0, 0, 0)
+        self.superuser = util_helper.generate_test_user(is_superuser=True)
+        self.account1 = account_helper.generate_aws_account(user=self.user1)
+        self.account1.created_at = util_helper.utc_dt(2017, 12, 1, 0, 0, 0)
+        self.account1.save()
+        self.account2 = account_helper.generate_aws_account(user=self.user1)
+        self.account2.created_at = util_helper.utc_dt(2017, 12, 1, 0, 0, 0)
+        self.account2.save()
+        self.account3 = account_helper.generate_aws_account(user=self.user2)
+        self.account3.created_at = util_helper.utc_dt(2017, 12, 1, 0, 0, 0)
+        self.account3.save()
+        self.account4 = account_helper.generate_aws_account(user=self.user2)
+        self.account4.created_at = util_helper.utc_dt(2017, 12, 1, 0, 0, 0)
+        self.account4.save()
+        self.instance1 = \
+            account_helper.generate_aws_instance(account=self.account1)
+        self.instance2 = \
+            account_helper.generate_aws_instance(account=self.account2)
+        self.instance3 = \
+            account_helper.generate_aws_instance(account=self.account3)
+        self.instance4 = \
+            account_helper.generate_aws_instance(account=self.account4)
+        self.windows_image = account_helper.generate_aws_image(
+            self.account1,
+            is_encrypted=False,
+            is_windows=True,
+        )
+        self.rhel_image = account_helper.generate_aws_image(
+            self.account2,
+            is_encrypted=False,
+            is_windows=False,
+            ec2_ami_id=None,
+            is_rhel=True,
+            is_openshift=False)
+        self.openshift_image = account_helper.generate_aws_image(
+            self.account3,
+            is_encrypted=False,
+            is_windows=False,
+            ec2_ami_id=None,
+            is_rhel=False,
+            is_openshift=True)
+        self.openshift_and_rhel_image = account_helper.generate_aws_image(
+            self.account4,
+            is_encrypted=False,
+            is_windows=False,
+            ec2_ami_id=None,
+            is_rhel=True,
+            is_openshift=True)
+        self.event1 = \
+            account_helper.generate_single_aws_instance_event(
+                instance=self.instance1, powered_time=powered_time,
+                event_type=InstanceEvent.TYPE.power_on,
+                ec2_ami_id=self.windows_image.ec2_ami_id)
+        self.event2 = \
+            account_helper.generate_single_aws_instance_event(
+                instance=self.instance2, powered_time=powered_time,
+                event_type=InstanceEvent.TYPE.power_on,
+                ec2_ami_id=self.rhel_image.ec2_ami_id)
+        self.event3 = \
+            account_helper.generate_single_aws_instance_event(
+                instance=self.instance3, powered_time=powered_time,
+                event_type=InstanceEvent.TYPE.power_on,
+                ec2_ami_id=self.openshift_image.ec2_ami_id)
+        self.event4 = \
+            account_helper.generate_single_aws_instance_event(
+                instance=self.instance4, powered_time=powered_time,
+                event_type=InstanceEvent.TYPE.power_on,
+                ec2_ami_id=self.openshift_and_rhel_image.ec2_ami_id)
+        self.factory = APIRequestFactory()
+        self.account1_expected_overview = {
+            'id': self.account1.aws_account_id,
+            'user_id': self.account1.user_id,
+            'type': 'aws',
+            'arn': self.account1.account_arn,
+            'creation_date': self.account1.created_at,
+            'name': self.account1.name,
+            'images': 1,
+            'instances': 1,
+            'rhel_instances': 0,
+            'openshift_instances': 0}
+        self.account2_expected_overview = {
+            'id': self.account2.aws_account_id,
+            'user_id': self.account2.user_id,
+            'type': 'aws',
+            'arn': self.account2.account_arn,
+            'creation_date': self.account2.created_at,
+            'name': self.account2.name,
+            'images': 1,
+            'instances': 1,
+            'rhel_instances': 1,
+            'openshift_instances': 0}
+        self.account3_expected_overview = {
+            'id': self.account3.aws_account_id,
+            'user_id': self.account3.user_id,
+            'type': 'aws',
+            'arn': self.account3.account_arn,
+            'creation_date': self.account3.created_at,
+            'name': self.account3.name,
+            'images': 1,
+            'instances': 1,
+            'rhel_instances': 0,
+            'openshift_instances': 1}
+        self.account4_expected_overview = {
+            'id': self.account4.aws_account_id,
+            'user_id': self.account4.user_id,
+            'type': 'aws',
+            'arn': self.account4.account_arn,
+            'creation_date': self.account4.created_at,
+            'name': self.account4.name,
+            'images': 1,
+            'instances': 1,
+            'rhel_instances': 1,
+            'openshift_instances': 1}
+
+    def get_overview_list_response(self, user, data=None):
+        """
+        Generate a response for a get-list on the InstanceEventViewSet.
+
+        Args:
+            user (User): Django auth user performing the request
+            data (dict): optional data to use as query params
+
+        Returns:
+            Response: the generated response for this request
+
+        """
+        if data is None:
+            # start and end date are required
+            data = {'start': self.start,
+                    'end': self.end
+                    }
+
+        request = self.factory.get('/report/accounts/', data)
+        force_authenticate(request, user=user)
+        view = CloudAccountOverviewViewSet.as_view(actions={'get': 'list'})
+        response = view(request)
+        return response
+
+    def test_list_overviews_as_superuser(self):
+        """Assert that the superuser sees all overviews regardless of owner."""
+        expected_response = {
+            'cloud_account_overviews': [
+                self.account1_expected_overview,
+                self.account2_expected_overview,
+                self.account3_expected_overview,
+                self.account4_expected_overview
+            ]
+        }
+        response = self.get_overview_list_response(self.superuser)
+        actual_response = response.data
+        self.assertEqual(expected_response, actual_response)
+
+    def test_list_overviews_as_user1(self):
+        """Assert that the user1 sees only overviews of user1 accounts."""
+        expected_response = {
+            'cloud_account_overviews': [
+                self.account1_expected_overview,
+                self.account2_expected_overview,
+            ]
+        }
+        response = self.get_overview_list_response(self.user1)
+        actual_response = response.data
+        self.assertEqual(expected_response, actual_response)
+
+    def test_list_overviews_as_user2(self):
+        """Assert that the user2 sees only overviews of user2 accounts."""
+        expected_response = {
+            'cloud_account_overviews': [
+                self.account3_expected_overview,
+                self.account4_expected_overview,
+            ]
+        }
+        response = self.get_overview_list_response(self.user2)
+        actual_response = response.data
+        self.assertEqual(expected_response, actual_response)
+
+    def test_get_user1s_overview_as_superuser_returns_ok(self):
+        """Assert that superuser can get another user's overviews."""
+        expected_response = {
+            'cloud_account_overviews': [
+                self.account1_expected_overview,
+                self.account2_expected_overview,
+            ]
+        }
+        params = {'user_id': self.user1.id,
+                  'start': self.start,
+                  'end': self.end
+                  }
+        response = self.get_overview_list_response(self.superuser, params)
+        actual_response = response.data
+        self.assertEqual(expected_response, actual_response)
+
+    def test_list_overviews_as_superuser_with_bad_filter(self):
+        """Assert that the list overviews returns 400 with bad start & end."""
+        params = {'start': 'January 1',
+                  'end': 'February 1'}
+        response = self.get_overview_list_response(self.superuser, params)
+        self.assertEqual(response.status_code, 400)
