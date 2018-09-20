151686a2291d164827382088726b48678ed87005
Brad Smith <brasmith@redhat.com>
2018-09-20 16:08:54 -0400

    add "configurequeues" management command to ensure queues exist with DLQ redrive policies

---
+++ cloudigrade/util/management/commands/configurequeues.py
@@ -0,0 +1,23 @@
+"""Ensure SQS queues are correctly configured for cloudigrade."""
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
+
+from util import aws
+
+
+class Command(BaseCommand):
+    """Ensure SQS queues are correctly configured for cloudigrade."""
+
+    help = _('Ensures SQS queues are correctly configured for cloudigrade.')
+
+    queue_names = [
+        '{0}ready_volumes'.format(settings.AWS_NAME_PREFIX),
+        settings.HOUNDIGRADE_RESULTS_QUEUE_NAME,
+    ]
+
+    def handle(self, *args, **options):
+        """Handle the command execution."""
+        for queue_name in self.queue_names:
+            queue_url = aws.get_sqs_queue_url(queue_name)
+            aws.ensure_queue_has_dql(queue_name, queue_url)
---
+++ cloudigrade/util/tests/test_management_commands_configurequeues.py
@@ -0,0 +1,51 @@
+"""Collection of tests for the 'configurequeues' management command."""
+from unittest.mock import PropertyMock, call, patch
+
+import faker
+from django.core.management import call_command
+from django.test import TestCase
+
+_faker = faker.Faker()
+
+
+class ConfigureQueuesTest(TestCase):
+    """Management command 'configurequeues' test case."""
+
+    def test_command_output(self):
+        """
+        Test that 'configurequeues' ensures its list of queues have DLQs.
+
+        Note:
+            This patches over the built-in queue names with dummy test names.
+        """
+        queue_names = [
+            _faker.slug(),
+            _faker.slug(),
+            _faker.slug(),
+        ]
+        queue_urls = {
+            queue_name: _faker.url() for queue_name in queue_names
+        }
+        expected_get_url_calls = [
+            call(queue_name) for queue_name in queue_names
+        ]
+        expected_ensure_queue_has_dql_calls = [
+            call(name, url) for name, url in queue_urls.items()
+        ]
+
+        def fake_get_sqs_queue_url(name):
+            """Fake calls for aws.get_sqs_queue_url."""
+            return queue_urls[name]
+
+        _path = 'util.management.commands.configurequeues'
+        with patch('{}.aws'.format(_path)) as mock_aws, \
+                patch('{}.Command.queue_names'.format(_path),
+                      new_callable=PropertyMock) as mock_queue_names:
+            mock_queue_names.return_value = queue_names
+            mock_aws.get_sqs_queue_url.side_effect = fake_get_sqs_queue_url
+
+            call_command('configurequeues')
+
+            mock_aws.get_sqs_queue_url.assert_has_calls(expected_get_url_calls)
+            mock_aws.ensure_queue_has_dql.assert_has_calls(
+                expected_ensure_queue_has_dql_calls)
