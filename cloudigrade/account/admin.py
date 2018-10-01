4f25acdfcdfc841d9d2f50d98017ecab79591b30
Brad Smith <brasmith@redhat.com>
2018-10-01 16:45:59 -0400

    add details to admin list display to ease occasional admin investigations

--- cloudigrade/account/admin.py
+++ cloudigrade/account/admin.py
@@ -10,12 +10,94 @@ from account.models import (Account,
                             InstanceEvent,
                             MachineImage)
 
-admin.site.register(Account, admin.ModelAdmin)
-admin.site.register(Instance, admin.ModelAdmin)
-admin.site.register(InstanceEvent, admin.ModelAdmin)
-admin.site.register(MachineImage, admin.ModelAdmin)
 
-admin.site.register(AwsAccount, admin.ModelAdmin)
-admin.site.register(AwsInstance, admin.ModelAdmin)
-admin.site.register(AwsInstanceEvent, admin.ModelAdmin)
-admin.site.register(AwsMachineImage, admin.ModelAdmin)
+class AccountAdmin(admin.ModelAdmin):
+    """Admin model of Account."""
+
+    list_display = (
+        'id',
+        'name',
+        'user',
+        'user_id',
+    )
+
+
+class InstanceAdmin(admin.ModelAdmin):
+    """Admin model of Instance."""
+
+    list_display = (
+        'id',
+        'account',
+    )
+
+
+class InstanceEventAdmin(admin.ModelAdmin):
+    """Admin model of InstanceEvent."""
+
+    list_display = (
+        'id',
+        'instance',
+        'machineimage',
+        'event_type',
+        'occurred_at',
+    )
+
+
+class MachineImageAdmin(admin.ModelAdmin):
+    """Admin model of MachineImage."""
+
+    list_display = (
+        'id',
+        'name',
+        'status',
+    )
+
+
+class AwsAccountAdmin(admin.ModelAdmin):
+    """Admin model of AwsAccount."""
+
+    list_display = AccountAdmin.list_display + (
+        'aws_account_id',
+        'account_arn',
+    )
+
+
+class AwsInstanceAdmin(admin.ModelAdmin):
+    """Admin model of AwsInstance."""
+
+    list_display = InstanceAdmin.list_display + (
+        'ec2_instance_id',
+        'region',
+    )
+
+
+class AwsInstanceEventAdmin(admin.ModelAdmin):
+    """Admin model of AwsInstanceEvent."""
+
+    list_display = InstanceEventAdmin.list_display + (
+        'subnet',
+        'instance_type',
+    )
+
+
+class AwsMachineImageAdmin(admin.ModelAdmin):
+    """Admin model of AwsMachineImage."""
+
+    list_display = MachineImageAdmin.list_display + (
+        'ec2_ami_id',
+        'platform',
+        'owner_aws_account_id',
+        'rhel_challenged',
+        'openshift_challenged',
+    )
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Instance, InstanceAdmin)
+admin.site.register(InstanceEvent, InstanceEventAdmin)
+admin.site.register(MachineImage, MachineImageAdmin)
+
+admin.site.register(AwsAccount, AwsAccountAdmin)
+admin.site.register(AwsInstance, AwsInstanceAdmin)
+admin.site.register(AwsInstanceEvent, AwsInstanceEventAdmin)
+admin.site.register(AwsMachineImage, AwsMachineImageAdmin)
