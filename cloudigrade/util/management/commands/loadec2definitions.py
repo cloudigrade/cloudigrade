2568eee77ee507ceea5887bbc315ee393c841aa2
Brad Smith <brasmith@redhat.com>
2018-10-07 23:59:28 -0400

    add management command for loading aws ec2 definitions

---
+++ cloudigrade/util/management/commands/loadec2definitions.py
@@ -0,0 +1,36 @@
+"""
+Trigger the async task to load EC2 instance definitions.
+
+Note:
+    Unless `--force` flag is given, this task checks if any definitions already
+    exist, and it will only call the task if none are found.
+"""
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
+
+from account.models import AwsEC2InstanceDefinitions
+from analyzer import tasks
+
+
+class Command(BaseCommand):
+    """Trigger the async task to load EC2 instance definitions."""
+
+    help = _('Load AWS EC2 instance definitions if none are present.')
+
+    def add_arguments(self, parser):
+        """Add command-line arguments."""
+        parser.add_argument('--force', action='store_true', help=_(
+            'force load definitions even if some are already present'
+        ))
+
+    def handle(self, *args, **options):
+        """Handle the command execution."""
+        if not options['force'] and AwsEC2InstanceDefinitions.objects.exists():
+            self.stdout.write(_(
+                'Nothing to do. EC2 instance definitions already exist.'
+            ))
+            return
+        tasks.repopulate_ec2_instance_mapping.delay()
+        self.stdout.write(_(
+            'Async task launched to load EC2 instance definitions.'
+        ))
