d3fcff9fdc5bd6260b5ab2abe7073345ad5657ca
Brad Smith <brasmith@redhat.com>
2019-03-11 14:22:14 -0400

    create runs from events for each instance, not *all* events at once

--- cloudigrade/util/management/commands/create_runs.py
+++ cloudigrade/util/management/commands/create_runs.py
@@ -3,7 +3,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from account.models import AwsInstanceEvent, Run
+from account.models import AwsInstanceEvent, Run, AwsInstance
 from account.reports import normalize_runs
 
 logger = logging.getLogger(__name__)
@@ -14,20 +14,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Handle the command execution."""
-        events = AwsInstanceEvent.objects.all()
+        for instance in AwsInstance.objects.all():
+            logger.info('Processing events for instance {}'.format(instance))
+            events = AwsInstanceEvent.objects.filter(instance=instance)
 
-        normalized_runs = normalize_runs(events)
+            normalized_runs = normalize_runs(events)
 
-        for index, normalized_run in enumerate(normalized_runs):
-            logger.info('Processing run {} of {}'.format(
-                index + 1, len(normalized_runs)))
-            run = Run(
-                start_time=normalized_run.start_time,
-                end_time=normalized_run.end_time,
-                machineimage_id=normalized_run.image_id,
-                instance_id=normalized_run.instance_id,
-                instance_type=normalized_run.instance_type,
-                memory=normalized_run.instance_memory,
-                vcpu=normalized_run.instance_vcpu,
-            )
-            run.save()
+            for index, normalized_run in enumerate(normalized_runs):
+                logger.info(
+                    'Processing run {} of {}'.format(
+                        index + 1, len(normalized_runs)
+                    )
+                )
+                run = Run(
+                    start_time=normalized_run.start_time,
+                    end_time=normalized_run.end_time,
+                    machineimage_id=normalized_run.image_id,
+                    instance_id=normalized_run.instance_id,
+                    instance_type=normalized_run.instance_type,
+                    memory=normalized_run.instance_memory,
+                    vcpu=normalized_run.instance_vcpu,
+                )
+                run.save()
