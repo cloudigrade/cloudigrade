d41f973d91beccdeaaee57f44dda081ce34111a0
Brad Smith <brasmith@redhat.com>
2019-04-08 16:21:54 -0400

    log specific error when configurequeues fails on the cloudtrail event queue

--- cloudigrade/util/management/commands/configurequeues.py
+++ cloudigrade/util/management/commands/configurequeues.py
@@ -1,9 +1,13 @@
 """Ensure SQS queues are correctly configured for cloudigrade."""
+import logging
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext as _
 
 from util import aws
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -28,4 +32,15 @@ class Command(BaseCommand):
         for queue_url in self.queue_urls:
             queue_name = queue_url.split('/')[-1]
             self.stdout.write('Configuring SQS queue "{}"'.format(queue_name))
-            aws.ensure_queue_has_dlq(queue_name, queue_url)
+            try:
+                aws.ensure_queue_has_dlq(queue_name, queue_url)
+            except:  # noqa: E722
+                logger.error(
+                    _(
+                        'Failed to configure queue %(name)s at %(url)s. '
+                        'Please verify that a queue exists in this location. '
+                        'We may not create this queue automatically!'
+                    ),
+                    {'name': queue_name, 'url': queue_url},
+                )
+                raise
