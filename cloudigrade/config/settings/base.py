0257ba44884c368e0a72b0b52b2b6fa5b0b7e2f1
Brad Smith <brasmith@redhat.com>
2019-05-20 10:10:07 -0400

    clear the celery beat schedule config

--- cloudigrade/config/settings/base.py
+++ cloudigrade/config/settings/base.py
@@ -353,34 +353,7 @@ CELERY_TASK_ROUTES = {
     'analyzer.tasks.repopulate_ec2_instance_mapping':
         {'queue': 'repopulate_ec2_instance_mapping'},
 }
-CELERY_BEAT_SCHEDULE = {
-    'scale_up_inspection_cluster_every_60_min': {
-        'task': 'account.tasks.scale_up_inspection_cluster',
-        # seconds
-        'schedule': env.int('SCALE_UP_INSPECTION_CLUSTER_SCHEDULE', default=60 * 60),
-    },
-    'persist_inspection_cluster_results': {
-        'task': 'account.tasks.persist_inspection_cluster_results_task',
-        # seconds
-        'schedule': env.int('PERSIST_INSPECTION_CLUSTER_RESULTS_SCHEDULE', default=5 * 60),
-    },
-    'analyze_log_every_2_mins': {
-        'task': 'analyzer.tasks.analyze_log',
-        # seconds
-        'schedule': env.int('ANALYZE_LOG_SCHEDULE', default=2 * 60),
-    },
-    'inspect_pending_images': {
-        'task': 'account.tasks.inspect_pending_images',
-        'schedule': env.int('INSPECT_PENDING_IMAGES_SCHEDULE', default=15 * 60),
-    },
-    'repopulate_ec2_instance_mapping_every_week': {
-        'task': 'analyzer.tasks.repopulate_ec2_instance_mapping',
-        'schedule': env.int(
-            'REPOPULATE_EC2_INSTANCE_MAPPING_SCHEDULE',
-            default=60 * 60 * 24 * 7  # 1 week in seconds
-        ),
-    },
-}
+CELERY_BEAT_SCHEDULE = {}
 
 # Misc Config Values
 CLOUDIGRADE_VERSION = env('CLOUDIGRADE_VERSION', default=None)
