cea7067d63b9c972ea983d492e74883b69809da3
Brad Smith <brasmith@redhat.com>
2020-11-02 12:49:59 -0500

    black reformat

--- cloudigrade/api/migrations/0035_usertasklock.py
+++ cloudigrade/api/migrations/0035_usertasklock.py
@@ -9,22 +9,36 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('api', '0034_remove_cloudaccount_platform_endpoint_id'),
+        ("api", "0034_remove_cloudaccount_platform_endpoint_id"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='UserTaskLock',
+            name="UserTaskLock",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('locked', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("locked", models.BooleanField(default=False)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
             options={
-                'ordering': ('created_at',),
-                'abstract': False,
+                "ordering": ("created_at",),
+                "abstract": False,
             },
         ),
     ]
