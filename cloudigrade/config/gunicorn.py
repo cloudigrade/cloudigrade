1af0b82a364253036571be2237347e28ffb36306
Brad Smith <brasmith@redhat.com>
2020-12-01 16:53:36 -0500

    Revert "increase gunicorn workers to (cpu_count * 2) + 1 per recommendations"
    This reverts commit 46f6328036e2fab98865696dab9e723d684dcade.

--- cloudigrade/config/gunicorn.py
+++ cloudigrade/config/gunicorn.py
@@ -1,13 +1,4 @@
 """Gunicorn configuration file."""
 timeout = 120
 bind = "unix:/var/run/cloudigrade/gunicorn.sock"
-
-# https://docs.gunicorn.org/en/stable/design.html#how-many-workers
-# "Generally we recommend (2 x $num_cores) + 1 as the number of workers"
-try:
-    import os
-
-    cpu_count = os.cpu_count()
-except:  # noqa: E722
-    cpu_count = 1
-workers = (cpu_count * 2) + 1
+workers = 2
