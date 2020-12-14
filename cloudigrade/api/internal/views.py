9e3ed5d1eb1571801c50fa86dc1303afa3a28138
Brad Smith <brasmith@redhat.com>
2020-12-14 12:00:10 -0500

    grant broad access to internal apis, no 3scale header required

--- cloudigrade/api/internal/views.py
+++ cloudigrade/api/internal/views.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.models import User
 from django_filters import rest_framework as django_filters
-from rest_framework import exceptions, status, viewsets
+from rest_framework import exceptions, permissions, status, viewsets
 from rest_framework.decorators import action, api_view, authentication_classes, schema
 from rest_framework.response import Response
 
@@ -34,75 +34,73 @@ def availability_check(request):
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class InternalUserViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalViewSetMixin:
+    """Mixin of common attributes for our internal ViewSet classes."""
+
+    authentication_classes = []
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [django_filters.DjangoFilterBackend]
+    schema = None
+
+
+class InternalUserViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list Users for internal use."""
 
     queryset = User.objects.all()
     serializer_class = serializers.InternalUserSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "username": ["exact"],
         "date_joined": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalUserTaskLockViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalUserTaskLockViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list UserTaskLocks for internal use."""
 
     queryset = models.UserTaskLock.objects.all()
     serializer_class = serializers.InternalUserTaskLockSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "user": ["exact"],
         "locked": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalCloudAccountViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalCloudAccountViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list CloudAccounts for internal use."""
 
     queryset = models.CloudAccount.objects.all()
     serializer_class = serializers.InternalCloudAccountSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_class = filters.InternalCloudAccountFilterSet
-    schema = None
 
 
-class InternalInstanceViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalInstanceViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list Instances for internal use."""
 
     queryset = models.Instance.objects.all()
     serializer_class = serializers.InternalInstanceSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_class = filters.InternalInstanceFilterSet
-    schema = None
 
 
-class InternalInstanceEventViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalInstanceEventViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list InstanceEvents for internal use."""
 
     queryset = models.InstanceEvent.objects.all()
     serializer_class = serializers.InternalInstanceEventSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "event_type": ["exact"],
         "instance": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalMachineImageViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve, reinspect, or list MachineImages for internal use."""
 
     queryset = models.MachineImage.objects.all()
     serializer_class = serializers.InternalMachineImageSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "architecture": ["exact"],
         "name": ["exact"],
@@ -110,7 +108,6 @@ class InternalMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
     @action(detail=True, methods=["post"])
     def reinspect(self, request, pk=None):
@@ -126,46 +123,45 @@ class InternalMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
         return Response(serializer.data)
 
 
-class InternalRunViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalRunViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list Runs for internal use."""
 
     queryset = models.Run.objects.all()
     serializer_class = serializers.InternalRunSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_class = filters.InternalRunFilterSet
-    schema = None
 
 
-class InternalMachineImageInspectionStartViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalMachineImageInspectionStartViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list MachineImageInspectionStarts for internal use."""
 
     queryset = models.MachineImageInspectionStart.objects.all()
     serializer_class = serializers.InternalMachineImageInspectionStartSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "machineimage": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalConcurrentUsageViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalConcurrentUsageViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list ConcurrentUsages for internal use."""
 
     queryset = models.ConcurrentUsage.objects.all()
     serializer_class = serializers.InternalConcurrentUsageSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_class = filters.InternalConcurrentUsageFilterSet
-    schema = None
 
 
-class InternalConcurrentUsageCalculationTaskViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalConcurrentUsageCalculationTaskViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list ConcurrentUsageCalculationTasks for internal use."""
 
     queryset = models.ConcurrentUsageCalculationTask.objects.all()
     serializer_class = serializers.InternalConcurrentUsageCalculationTaskSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "status": ["exact"],
         "user": ["exact"],
@@ -174,15 +170,15 @@ class InternalConcurrentUsageCalculationTaskViewSet(viewsets.ReadOnlyModelViewSe
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalInstanceDefinitionViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalInstanceDefinitionViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list ConcurrentUsages for internal use."""
 
     queryset = models.InstanceDefinition.objects.all()
     serializer_class = serializers.InternalInstanceDefinitionSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "cloud_type": ["exact"],
         "instance_type": ["exact"],
@@ -191,55 +187,53 @@ class InternalInstanceDefinitionViewSet(viewsets.ReadOnlyModelViewSet):
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAwsCloudAccountViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAwsCloudAccountViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AwsCloudAccount for internal use."""
 
     queryset = aws_models.AwsCloudAccount.objects.all()
     serializer_class = serializers.InternalAwsCloudAccountSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_class = filters.InternalAwsCloudAccountFilterSet
-    schema = None
 
 
-class InternalAwsInstanceViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAwsInstanceViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list AwsInstances for internal use."""
 
     queryset = aws_models.AwsInstance.objects.all()
     serializer_class = serializers.InternalAwsInstanceSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "ec2_instance_id": ["exact"],
         "region": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAwsInstanceEventViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAwsInstanceEventViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AwsInstanceEvents for internal use."""
 
     queryset = aws_models.AwsInstanceEvent.objects.all()
     serializer_class = serializers.InternalAwsInstanceEventSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "subnet": ["exact"],
         "instance_type": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAwsMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAwsMachineImageViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AwsMachineImages for internal use."""
 
     queryset = aws_models.AwsMachineImage.objects.all()
     serializer_class = serializers.InternalAwsMachineImageSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "ec2_ami_id": ["exact"],
         "platform": ["exact"],
@@ -249,15 +243,15 @@ class InternalAwsMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAwsMachineImageCopyViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAwsMachineImageCopyViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AwsMachineImageCopies for internal use."""
 
     queryset = aws_models.AwsMachineImageCopy.objects.all()
     serializer_class = serializers.InternalAwsMachineImageCopySerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "reference_awsmachineimage": ["exact"],
         "ec2_ami_id": ["exact"],
@@ -268,54 +262,52 @@ class InternalAwsMachineImageCopyViewSet(viewsets.ReadOnlyModelViewSet):
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAzureCloudAccountViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAzureCloudAccountViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AzureMachineImages for internal use."""
 
     queryset = azure_models.AzureCloudAccount.objects.all()
     serializer_class = serializers.InternalAzureCloudAccountSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_class = filters.InternalAzureCloudAccountFilterSet
-    schema = None
 
 
-class InternalAzureInstanceViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAzureInstanceViewSet(InternalViewSetMixin, viewsets.ReadOnlyModelViewSet):
     """Retrieve or list AzureInstanceEvents for internal use."""
 
     queryset = azure_models.AzureInstance.objects.all()
     serializer_class = serializers.InternalAzureInstanceSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "region": ["exact"],
         "resource_id": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAzureInstanceEventViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAzureInstanceEventViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AzureInstanceEvents for internal use."""
 
     queryset = azure_models.AzureInstanceEvent.objects.all()
     serializer_class = serializers.InternalAzureInstanceEventSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "instance_type": ["exact"],
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
 
 
-class InternalAzureMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
+class InternalAzureMachineImageViewSet(
+    InternalViewSetMixin, viewsets.ReadOnlyModelViewSet
+):
     """Retrieve or list AzureMachineImages for internal use."""
 
     queryset = azure_models.AzureMachineImage.objects.all()
     serializer_class = serializers.InternalAzureMachineImageSerializer
-    filter_backends = [django_filters.DjangoFilterBackend]
     filterset_fields = {
         "azure_marketplace_image": ["exact"],
         "region": ["exact"],
@@ -323,4 +315,3 @@ class InternalAzureMachineImageViewSet(viewsets.ReadOnlyModelViewSet):
         "created_at": ["lt", "exact", "gt"],
         "updated_at": ["lt", "exact", "gt"],
     }
-    schema = None
