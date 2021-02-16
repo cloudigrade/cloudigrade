5bb977de57793c9e02abc86a03954862b413201e
Katherine Black <kblack@redhat.com>
2021-02-16 14:22:44 -0500

    Update deploy script to pull from ghcr and copy to quay.

--- deployment/scripts/build_deploy.sh
+++ deployment/scripts/build_deploy.sh
@@ -2,8 +2,19 @@
 
 set -exv
 
-IMAGE_NAME="quay.io/cloudservices/cloudigrade"
-IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+GIT_TAG=$(git tag --contains | head -1)
+GIT_TAG=${GIT_TAG:-latest}
+echo GIT_TAG
+
+IMAGE_TAG=$(git rev-parse HEAD)
+SHORT_IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+GITHUB_IMAGE_NAME=ghcr.io/cloudigrade/cloudigrade
+GITHUB_NGINX_IMAGE_NAME=ghcr.io/cloudigrade/cloudinginx
+
+IMAGE_NAME=quay.io/cloudservices/cloudigrade
+NGINX_IMAGE_NAME=quay.io/cloudservices/cloudinginx
+
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
@@ -13,7 +24,15 @@ fi
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-docker --config="$DOCKER_CONF" build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
-docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" pull "${GITHUB_IMAGE_NAME}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" pull "${GITHUB_NGINX_IMAGE_NAME}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" tag "${GITHUB_IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:${GIT_TAG}"
+docker --config="$DOCKER_CONF" tag "${GITHUB_IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:${SHORT_IMAGE_TAG}"
+docker --config="$DOCKER_CONF" tag "${GITHUB_NGINX_IMAGE_NAME}:${IMAGE_TAG}" "${NGINX_IMAGE_NAME}:${GIT_TAG}"
+docker --config="$DOCKER_CONF" tag "${GITHUB_NGINX_IMAGE_NAME}:${IMAGE_TAG}" "${NGINX_IMAGE_NAME}:${SHORT_IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${GIT_TAG}"
+docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${SHORT_IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${NGINX_IMAGE_NAME}:${GIT_TAG}"
+docker --config="$DOCKER_CONF" push "${NGINX_IMAGE_NAME}:${SHORT_IMAGE_TAG}"
 # docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
 # docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
