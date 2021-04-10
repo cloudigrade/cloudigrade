7d27241b38a7b4da8e6ea72711f3aaf09f71e80f
Brad Smith <brasmith@redhat.com>
2021-04-09 20:23:22 -0400

    rearrange calculate_max_concurrent_usage_task logic so related DB calls are in transaction
    also update any "RUNNING" tasks to "SCHEDULED" since we no longer look for the former status.

--- cloudigrade/api/tests/tasks/test_calculate_max_concurrent_usage_task.py
+++ cloudigrade/api/tests/tasks/test_calculate_max_concurrent_usage_task.py
@@ -30,118 +30,116 @@ class CalculateMaxConcurrentUsageTaskTest(TestCase):
         )
         api_helper.generate_instance_type_definitions()
 
-    def test_calculate_max_concurrent_usage_task(self):
-        """Test ConcurrentUsageCalculationTask is set to complete."""
+    @patch("api.tasks.calculate_max_concurrent_usage")
+    def test_calculate_max_concurrent_usage_task(
+        self, mock_calculate_max_concurrent_usage
+    ):
+        """
+        Test happy path behavior.
+
+        The calculation function should be called and the task is set to complete.
+        """
         calculate_max_concurrent_usage_task.retry = MagicMock(side_effect=Retry)
 
         task_id = uuid4()
+        user_id = self.user.id
         request_date = datetime.date(2019, 5, 1)
         concurrent_task = ConcurrentUsageCalculationTask(
-            user_id=self.user.id, date=request_date, task_id=task_id
+            user_id=user_id, date=request_date, task_id=task_id
         )
         concurrent_task.save()
-
         calculate_max_concurrent_usage_task.push_request(id=task_id)
+        calculate_max_concurrent_usage_task.run(str(request_date), user_id)
 
-        calculate_max_concurrent_usage_task.run(str(request_date), self.user.id)
+        mock_calculate_max_concurrent_usage.assert_called_with(request_date, user_id)
         self.assertEqual(
             ConcurrentUsageCalculationTask.objects.get(task_id=task_id).status,
             ConcurrentUsageCalculationTask.COMPLETE,
         )
 
-    def test_task_retry_if_another_task_is_running(self):
-        """Test that task retries if another task is running with same user and date."""
-        calculate_max_concurrent_usage_task.retry = MagicMock(side_effect=Retry)
+    @patch("api.tasks.calculate_max_concurrent_usage")
+    def test_return_if_user_does_not_exist(self, mock_calculate_max_concurrent_usage):
+        """
+        Test that the task returns early if user does not exist.
 
-        request_date = datetime.date(2019, 5, 1)
-        concurrent_task = ConcurrentUsageCalculationTask(
-            user_id=self.user.id, date=request_date, task_id=uuid4()
-        )
-        concurrent_task.status = ConcurrentUsageCalculationTask.RUNNING
-        concurrent_task.save()
-
-        with self.assertRaises(Retry):
-            calculate_max_concurrent_usage_task(str(request_date), self.user.id)
-
-    def test_task_cancel_if_newer_task_is_scheduled(self):
-        """Test cancel if newer scheduled task is scheduled with same user and date."""
+        The calculate function should not be called.
+        """
         task_id = uuid4()
         request_date = datetime.date(2019, 5, 1)
-        concurrent_task = ConcurrentUsageCalculationTask(
-            user_id=self.user.id, date=request_date, task_id=task_id
-        )
-        concurrent_task.save()
+        calculate_max_concurrent_usage_task.push_request(id=task_id)
+        calculate_max_concurrent_usage_task.run(str(request_date), _faker.pyint())
+        mock_calculate_max_concurrent_usage.assert_not_called()
 
-        newer_task = ConcurrentUsageCalculationTask(
-            user_id=self.user.id, date=request_date, task_id=uuid4()
-        )
-        newer_task.created_at = concurrent_task.created_at + datetime.timedelta(days=1)
-        newer_task.save()
-
-        concurrent_task.save = MagicMock()
-
-        with patch.object(ConcurrentUsageCalculationTask, "cancel") as mock_cancel:
-            calculate_max_concurrent_usage_task.push_request(id=task_id)
-            calculate_max_concurrent_usage_task.run(str(request_date), self.user.id)
-
-        mock_cancel.assert_called()
-
-        newer_task.refresh_from_db()
-        self.assertEqual(
-            concurrent_task.status, ConcurrentUsageCalculationTask.SCHEDULED
-        )
-        concurrent_task.save.assert_not_called()
-
+    @patch("api.tasks.calculate_max_concurrent_usage")
     @patch("api.tasks.schedule_concurrent_calculation_task")
-    def test_new_task_if_existing_task_is_missing(self, mock_schedule_task):
-        """Test that task spawns a new task with same user and date if missing."""
+    def test_new_task_if_existing_task_is_missing(
+        self, mock_schedule_task, mock_calculate_max_concurrent_usage
+    ):
+        """
+        Test that the task schedules a new task with same user and date if missing.
+
+        The calculate function should not be called.
+        """
         task_id = uuid4()
+        user_id = self.user.id
         request_date = datetime.date(2019, 5, 1)
         calculate_max_concurrent_usage_task.push_request(id=task_id)
 
         with self.assertLogs("api.tasks", level="WARNING") as logging_watcher:
-            calculate_max_concurrent_usage_task.run(str(request_date), self.user.id)
-            mock_schedule_task.assert_called_with(request_date, self.user.id)
+            calculate_max_concurrent_usage_task.run(str(request_date), user_id)
+
+        mock_schedule_task.assert_called_with(request_date, user_id)
+        mock_calculate_max_concurrent_usage.assert_not_called()
         self.assertIn(
-            f'ConcurrentUsageCalculationTask not found for task ID "{task_id}"',
+            f"ConcurrentUsageCalculationTask not found for task ID {task_id}",
             logging_watcher.output[0],
         )
 
     @patch("api.tasks.calculate_max_concurrent_usage")
-    def test_invalid_user(self, mock_calculate_max_concurrent_usage):
-        """Test that task exits early if user does not exist."""
+    def test_early_return_on_unexpected_status(
+        self, mock_calculate_max_concurrent_usage
+    ):
+        """Test early return if task status is not SCHEDULED."""
         task_id = uuid4()
+        user_id = self.user.id
         request_date = datetime.date(2019, 5, 1)
         concurrent_task = ConcurrentUsageCalculationTask(
-            user_id=self.user.id, date=request_date, task_id=task_id
+            user_id=user_id,
+            date=request_date,
+            task_id=task_id,
+            status=ConcurrentUsageCalculationTask.COMPLETE,
         )
         concurrent_task.save()
 
         calculate_max_concurrent_usage_task.push_request(id=task_id)
-
-        calculate_max_concurrent_usage_task.run(request_date, _faker.pyint())
         mock_calculate_max_concurrent_usage.assert_not_called()
 
     @patch("api.tasks.calculate_max_concurrent_usage")
     def test_calculate_max_concurrent_usage_exception(
         self, mock_calculate_max_concurrent_usage
     ):
-        """Test that task is marked as ERROR if calculate_max_concurrent_usage fails."""
+        """
+        Test that task is marked as ERROR if calculate_max_concurrent_usage fails.
+
+        The calculate function is called here, but we force it to raise an exception.
+        """
         mock_calculate_max_concurrent_usage.side_effect = Exception()
 
         task_id = uuid4()
+        user_id = self.user.id
         request_date = datetime.date(2019, 5, 1)
         concurrent_task = ConcurrentUsageCalculationTask(
-            user_id=self.user.id, date=request_date, task_id=task_id
+            user_id=user_id, date=request_date, task_id=task_id
         )
         concurrent_task.save()
 
         calculate_max_concurrent_usage_task.push_request(id=task_id)
 
         with self.assertRaises(Exception):
-            calculate_max_concurrent_usage_task.run(str(request_date), self.user.id)
+            calculate_max_concurrent_usage_task.run(str(request_date), user_id)
 
         self.assertEqual(
             ConcurrentUsageCalculationTask.objects.get(task_id=task_id).status,
             ConcurrentUsageCalculationTask.ERROR,
         )
+        mock_calculate_max_concurrent_usage.assert_called_with(request_date, user_id)
