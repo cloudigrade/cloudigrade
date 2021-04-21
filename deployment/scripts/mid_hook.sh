a04343418339b9bb886441e3cc6c56c073416a7a
Katherine Black <kblack@redhat.com>
2021-04-21 13:41:06 -0700

    Send a slack notification with ansible playbook results.

--- deployment/scripts/mid_hook.sh
+++ deployment/scripts/mid_hook.sh
@@ -5,7 +5,10 @@ echo "ansible:x:$(id -u):$(id -g):,,,:${HOME}:/bin/bash" >> /etc/passwd
 echo "ansible:x:$(id -G | cut -d' ' -f 2)" >> /etc/group
 id
 
-ANSIBLE_CONFIG=/home/cloudigrade/playbooks/ansible.cfg ansible-playbook -e env=${CLOUDIGRADE_ENVIRONMENT} playbooks/manage-cloudigrade.yml
+ANSIBLE_CONFIG=/home/cloudigrade/playbooks/ansible.cfg ansible-playbook -e env=${CLOUDIGRADE_ENVIRONMENT} playbooks/manage-cloudigrade.yml | tee slack-payload
+
+slack_payload=`cat slack-payload | tail -n 3`
+curl -X POST --data-urlencode "payload={\"channel\": \"#cloudmeter-deployments-dev\", \"text\": \"$slack_payload\"}" ${SLACK_TOKEN}
 
 python3 ./manage.py configurequeues
 python3 ./manage.py syncbucketlifecycle
