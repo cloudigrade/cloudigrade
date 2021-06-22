81e4963bff6aeff3a52a4db698d92e601c008bbb
Brad Smith <brasmith@redhat.com>
2021-06-22 18:01:16 -0400

    make local get the real AWS account ID like prod

--- cloudigrade/config/settings/local.py
+++ cloudigrade/config/settings/local.py
@@ -1,6 +1,12 @@
 """Settings file meant for local development."""
+from boto3 import client
 from .base import *
 
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 SECRET_KEY = env("DJANGO_SECRET_KEY", default="local")
 INSTALLED_APPS = INSTALLED_APPS + ["django_extensions"]
+
+# Instead of calling util.aws.sts._get_primary_account_id,
+# we use boto here directly to avoid a potential import loop.
+account_id = client("sts").get_caller_identity().get("Account")
+AWS_CLOUDTRAIL_EVENT_URL = f"https://sqs.us-east-1.amazonaws.com/{account_id}/cloudigrade-cloudtrail-s3-{CLOUDIGRADE_ENVIRONMENT}"
