5b0e29ebdfe1002f2a10964ae55034a0f5c3ed8c
Brad Smith <brasmith@redhat.com>
2021-07-15 16:02:18 -0400

    call recalculate_runs directly from create_runs, reducing duplicate code

--- cloudigrade/util/tests/test_management_commands_create_runs.py
+++ cloudigrade/util/tests/test_management_commands_create_runs.py
@@ -6,7 +6,7 @@ from django.core.management import call_command
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
-from api.models import ConcurrentUsage, ConcurrentUsageCalculationTask, Run
+from api import models
 from api.tests import helper as api_helper
 from util.tests import helper as util_helper
 
@@ -36,31 +36,62 @@ class CreateRunsTest(TestCase):
             instance_type=self.instance_type,
         )
 
-    @patch("api.tasks.calculate_max_concurrent_usage_task")
-    def test_handle(self, mock_calculate_task):
+    def get_first_run(self):
+        """Get the Run with the first/oldest start time."""
+        return models.Run.objects.order_by("start_time").first()
+
+    def assertRunsEqual(self, run_a, run_b):
+        """Assert that two runs have effectively equal content."""
+        self.assertEqual(run_a.instance, run_b.instance)
+        self.assertEqual(run_a.start_time, run_b.start_time)
+        self.assertEqual(run_a.end_time, run_b.end_time)
+
+    def assertCreateRunsCompleted(self, old_run):
+        """Assert the create_runs command had expected effects on models."""
+        run = self.get_first_run()
+        self.assertRunsEqual(run, old_run)
+        with self.assertRaises(models.Run.DoesNotExist):
+            # expect this to fail because the old run should have been deleted.
+            old_run.refresh_from_db()
+        self.assertEqual(models.Run.objects.all().count(), 1)
+        self.assertEqual(models.ConcurrentUsage.objects.all().count(), 0)
+
+    def assertCreateRunsAborted(self, old_run):
+        """Assert the create_runs command had no effects on models due to abortion."""
+        run = self.get_first_run()
+        self.assertRunsEqual(run, old_run)
+        old_run.refresh_from_db()  # refresh should work; old run should not be deleted.
+        self.assertEqual(models.Run.objects.all().count(), 1)
+        self.assertEqual(models.ConcurrentUsage.objects.all().count(), 1)
+
+    @patch(
+        "util.management.commands.create_runs.calculate_max_concurrent_usage_from_runs"
+    )
+    def test_handle_confirm_flag(self, mock_calculate):
         """Test calling create_runs with confirm arg."""
+        old_run = self.get_first_run()
         call_command("create_runs", "--confirm")
-        self.assertEqual(Run.objects.all().count(), 1)
-        self.assertEqual(ConcurrentUsageCalculationTask.objects.all().count(), 1)
-        self.assertEqual(ConcurrentUsage.objects.all().count(), 0)
-        mock_calculate_task.apply_async.assert_called()
+        self.assertCreateRunsCompleted(old_run)
+        mock_calculate.assert_called_with([self.get_first_run()])
 
-    @patch("api.tasks.calculate_max_concurrent_usage_task")
-    @patch("builtins.input", return_value="N")
-    def test_handle_no(self, mock_input, mock_calculate_task):
-        """Test calling create_runs with no input."""
+    @patch(
+        "util.management.commands.create_runs.calculate_max_concurrent_usage_from_runs"
+    )
+    @patch("util.management.commands.create_runs.input", return_value="N")
+    def test_handle_input_no(self, mock_input, mock_calculate):
+        """Test calling create_runs with "N" no input."""
+        old_run = self.get_first_run()
         call_command("create_runs")
-        self.assertEqual(Run.objects.all().count(), 1)
-        self.assertEqual(ConcurrentUsageCalculationTask.objects.all().count(), 0)
-        self.assertEqual(ConcurrentUsage.objects.all().count(), 1)
-        mock_calculate_task.apply_async.assert_not_called()
+        self.assertCreateRunsAborted(old_run)
+        mock_calculate.assert_not_called()
 
-    @patch("api.tasks.calculate_max_concurrent_usage_task")
-    @patch("builtins.input", return_value="Y")
-    def test_handle_yes(self, mock_input, mock_calculate_task):
-        """Test calling create_runs with yes input."""
+    @patch(
+        "util.management.commands.create_runs.calculate_max_concurrent_usage_from_runs"
+    )
+    @patch("util.management.commands.create_runs.input", return_value="Y")
+    def test_handle_input_yes(self, mock_input, mock_calculate):
+        """Test calling create_runs with "Y" yes input."""
+        old_run = models.Run.objects.first()
         call_command("create_runs")
-        self.assertEqual(Run.objects.all().count(), 1)
-        self.assertEqual(ConcurrentUsageCalculationTask.objects.all().count(), 1)
-        self.assertEqual(ConcurrentUsage.objects.all().count(), 0)
-        mock_calculate_task.apply_async.assert_called()
+        self.assertCreateRunsCompleted(old_run)
+        mock_calculate.assert_called_with([self.get_first_run()])
