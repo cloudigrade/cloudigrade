b6d9767394320dfc9ebbe870330d5bd3f04f54be
Brad Smith <brasmith@redhat.com>
2021-07-27 11:37:23 -0400

    add optional CELERY_TASK_ALWAYS_EAGER override to local settings

--- cloudigrade/config/settings/local.py
+++ cloudigrade/config/settings/local.py
@@ -10,3 +10,10 @@ INSTALLED_APPS = INSTALLED_APPS + ["django_extensions"]
 # we use boto here directly to avoid a potential import loop.
 account_id = client("sts").get_caller_identity().get("Account")
 AWS_CLOUDTRAIL_EVENT_URL = f"https://sqs.us-east-1.amazonaws.com/{account_id}/cloudigrade-cloudtrail-s3-{CLOUDIGRADE_ENVIRONMENT}"
+
+# Only for local development allow setting Celery's "eager" option.
+# Setting this to "True" means Celery will never enqueue task messages for processing.
+# Instead it will always act as though the task function was called *directly*.
+# See also Celery docs:
+# https://docs.celeryproject.org/en/stable/userguide/configuration.html#std-setting-task_always_eager
+CELERY_TASK_ALWAYS_EAGER = env.bool("CELERY_TASK_ALWAYS_EAGER", default=False)
