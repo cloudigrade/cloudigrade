e0536f0c19b6c7ea2c458af24b1338f3b112f176
Katherine Black <kblack@redhat.com>
2021-08-17 13:58:21 -0700

    Handle snapshots that are already deleted.

--- cloudigrade/api/tests/clouds/aws/tasks/imageprep/test_delete_snapshot.py
+++ cloudigrade/api/tests/clouds/aws/tasks/imageprep/test_delete_snapshot.py
@@ -1,6 +1,7 @@
 """Collection of tests for aws.tasks.cloudtrail.delete_snapshot."""
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
+from botocore.exceptions import ClientError
 from django.conf import settings
 from django.test import TestCase
 
@@ -70,6 +71,35 @@ class DeleteSnapshotTest(TestCase):
 
         mock_ec2_client = mock_boto3.resource
         mock_snapshot = mock_ec2_client.return_value.Snapshot
+
+        delete_snapshot(mock_snapshot_copy_id, mock_ami_id, mock_region)
+
+        mock_ec2_client.assert_called_once_with("ec2")
+        mock_snapshot.assert_called_once_with(mock_snapshot_copy_id)
+        mock_snapshot.return_value.delete.assert_called_once()
+
+    @patch("api.clouds.aws.tasks.imageprep.AwsMachineImage")
+    @patch("api.clouds.aws.tasks.imageprep.boto3")
+    def test_delete_snapshot_missing(self, mock_boto3, mock_ami):
+        """Assert that the delete snapshot handles already deleted snapshots."""
+        mock_ami_id = util_helper.generate_dummy_image_id()
+
+        mock_image = account_helper.generate_image(
+            ec2_ami_id=mock_ami_id, status=MachineImage.INSPECTED
+        )
+        mock_snapshot_copy_id = util_helper.generate_dummy_snapshot_id()
+        mock_region = util_helper.get_random_region()
+
+        client_error = ClientError(
+            error_response={"Error": {"Code": "InvalidSnapshot.NotFound"}},
+            operation_name=Mock(),
+        )
+
+        mock_ami_get = mock_ami.objects.get
+        mock_ami_get.return_value.machine_image.get.return_value = mock_image
+        mock_ec2_client = mock_boto3.resource
+        mock_snapshot = mock_ec2_client.return_value.Snapshot
+        mock_snapshot.return_value.delete.side_effect = client_error
 
         delete_snapshot(mock_snapshot_copy_id, mock_ami_id, mock_region)
 
