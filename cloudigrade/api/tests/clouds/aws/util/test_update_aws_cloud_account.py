b53f1e54fc61f7bcb47e2ba1db28964d92146e10
Brad Smith <brasmith@redhat.com>
2021-08-31 14:12:55 -0400

    suppress unnecessary extra sources-api notification when replacing a CloudAccount, and make related test assertions more explicit

--- cloudigrade/api/tests/clouds/aws/util/test_update_aws_cloud_account.py
+++ cloudigrade/api/tests/clouds/aws/util/test_update_aws_cloud_account.py
@@ -45,14 +45,10 @@ class UpdateAWSClountTest(TestCase):
         )
         mock_notify_sources.delay.assert_called()
 
-    @patch("api.tasks.sources.notify_application_availability_task")
     @patch.object(CloudAccount, "disable")
     @patch.object(CloudAccount, "enable")
     def test_update_aws_clount_different_aws_account_id_success(
-        self,
-        mock_enable,
-        mock_disable,
-        mock_notify_sources,
+        self, mock_enable, mock_disable
     ):
         """Test update_aws_cloud_account works."""
         aws_account_id2 = util_helper.generate_dummy_aws_account_id()
@@ -66,55 +62,48 @@ class UpdateAWSClountTest(TestCase):
         self.assertTrue(AwsCloudAccount.objects.filter(account_arn=arn2).exists())
         self.assertEqual(0, Instance.objects.all().count())
 
+        mock_disable.assert_called()
+        mock_enable.assert_called()
+
     @patch("api.clouds.aws.util._notify_error_with_generic_message_for_different_user")
-    @patch("api.tasks.sources.notify_application_availability_task")
     @patch.object(CloudAccount, "disable")
     @patch.object(CloudAccount, "enable")
     def test_update_aws_clount_different_aws_account_id_fails_arn_already_exists(
-        self,
-        mock_enable,
-        mock_disable,
-        mock_notify_sources,
-        mock_notify_error,
+        self, mock_enable, mock_disable, mock_notify_error
     ):
         """Test update_aws_cloud_account fails for duplicate arn."""
         aws_account_id2 = util_helper.generate_dummy_aws_account_id()
         arn2 = util_helper.generate_dummy_arn(account_id=aws_account_id2)
 
-        api_helper.generate_cloud_account(
-            arn=arn2,
-            aws_account_id=aws_account_id2,
-        )
+        api_helper.generate_cloud_account(arn=arn2, aws_account_id=aws_account_id2)
 
         util.update_aws_cloud_account(
             self.clount, arn2, self.account_number, self.auth_id, self.source_id
         )
 
+        # The old CloudAccount should be disabled regardless of the new one's success.
+        mock_disable.assert_called()
         mock_notify_error.assert_called()
+        mock_enable.assert_not_called()
 
     @patch("api.clouds.aws.util._notify_error_with_generic_message_for_different_user")
-    @patch("api.tasks.sources.notify_application_availability_task")
     @patch.object(CloudAccount, "disable")
     @patch.object(CloudAccount, "enable")
     def test_update_aws_clount_different_aws_account_id_fails_account_id_already_exists(
-        self,
-        mock_enable,
-        mock_disable,
-        mock_notify_sources,
-        mock_notify_error,
+        self, mock_enable, mock_disable, mock_notify_error
     ):
         """Test update_aws_cloud_account fails for duplicate aws_account_id."""
         aws_account_id2 = util_helper.generate_dummy_aws_account_id()
         arn2 = util_helper.generate_dummy_arn(account_id=aws_account_id2)
         arn3 = util_helper.generate_dummy_arn(account_id=aws_account_id2)
 
-        api_helper.generate_cloud_account(
-            arn=arn2,
-            aws_account_id=aws_account_id2,
-        )
+        api_helper.generate_cloud_account(arn=arn2, aws_account_id=aws_account_id2)
 
         util.update_aws_cloud_account(
             self.clount, arn3, self.account_number, self.auth_id, self.source_id
         )
 
+        # The old CloudAccount should be disabled regardless of the new one's success.
+        mock_disable.assert_called()
         mock_notify_error.assert_called()
+        mock_enable.assert_not_called()
