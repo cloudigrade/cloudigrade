41ce6d89e3b0d0798645d652e473dcd261f486f5
Alberto Bellotti <abellott@redhat.com>
2022-07-23 02:22:57 -0400

    - Adding support for disconnecting from lighthouse upon account deletion. - Logging the list of the managed services and the matched lighthouse   registration definition.

--- cloudigrade/api/clouds/azure/models.py
+++ cloudigrade/api/clouds/azure/models.py
@@ -1,13 +1,18 @@
 """Cloudigrade API v2 Models for Azure."""
 import logging
 
+from azure.mgmt.managedservices import ManagedServicesClient
+from django.conf import settings
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models, transaction
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from django.utils.translation import gettext as _
 from rest_framework.exceptions import ValidationError
 
 from api import AZURE_PROVIDER_STRING
 from api.models import CloudAccount, Instance, InstanceEvent, MachineImage
+from util import azure
 from util.azure.identity import get_cloudigrade_available_subscriptions
 from util.models import BaseModel
 
@@ -245,4 +250,171 @@ class AzureInstanceEvent(BaseModel):
             f"created_at=parse({created_at}), "
             f"updated_at=parse({updated_at})"
             f")"
+        )
+
+
+@receiver(post_delete, sender=AzureCloudAccount)
+def delete_lighthouse_registration(*args, **kwargs):
+    """Delete the lighthouse registration upon deleting an Azure cloud account."""
+    logger.info(
+        _(
+            "Attempting delete_lighthouse_registration "
+            "for args=%(args)s kwargs=%(kwargs)s "
+        ),
+        {"args": args, "kwargs": kwargs},
+    )
+    azure_cloud_account = kwargs["instance"]
+    azure_subscription_id = azure_cloud_account.subscription_id
+    logger.info(
+        _("Attempting delete_lighthouse_registration for %(account)s"),
+        {"account": azure_cloud_account},
+    )
+    logger.info(
+        _("settings.AZURE_SUBSCRIPTION_ID %(subscription_id)s"),
+        {"subscription_id": settings.AZURE_SUBSCRIPTION_ID},
+    )
+    logger.info(
+        _("Azure Cloud Account subscription_id %(subscription_id)s"),
+        {"subscription_id": azure_subscription_id},
+    )
+
+    _delete_lighthouse_subscription(azure_subscription_id)
+
+
+def _delete_lighthouse_subscription(scoped_subscription_id):
+    """Delete the lighthouse registration for the given subscription id scope."""
+    ms_client = ManagedServicesClient(azure.get_cloudigrade_credentials())
+
+    # example
+    # "id": "/subscriptions/<subscription_id>/providers/
+    #           Microsoft.ManagedServices/registrationDefinitions/<reg_name>
+    # for the above id, the scope is subscriptions/<subscription_id>
+
+    ms_scope = f"subscriptions/{scoped_subscription_id}"
+
+    try:
+        rd_list = ms_client.registration_definitions.list(scope=ms_scope)
+        registration_name = None
+        found_rd = False
+        for ms_def in rd_list:
+            ms_definition = ms_def.as_dict()
+            name = ms_definition["name"]
+            properties = ms_definition["properties"]
+            managed_by_tenant_id = properties["managed_by_tenant_id"]
+            authorization = properties["authorizations"][0]
+            principal_id = authorization["principal_id"]
+            principal_id_display_name = authorization["principal_id_display_name"]
+            role_definition_id = authorization["role_definition_id"]
+            found_rd = True
+            logger.debug(
+                _(
+                    "\nFound a registration definition "
+                    "name=%(name)s, "
+                    "\nproperties=%(properties)s, "
+                    "\nmanaged_by_tenant_id=%(managed_by_tenant_id)s, "
+                    "\nprincipal_id=%(principal_id)s, "
+                    "\nprincipal_id_display_name=%(principal_id_display_name)s, "
+                    "\nrole_definition_id=%(role_definition_id)s",
+                ),
+                {
+                    "name": name,
+                    "properties": properties,
+                    "managed_by_tenant_id": managed_by_tenant_id,
+                    "principal_id": principal_id,
+                    "principal_id_display_name": principal_id_display_name,
+                    "role_definition_id": role_definition_id,
+                },
+            )
+            if (
+                managed_by_tenant_id == settings.AZURE_TENANT_ID
+                and principal_id == settings.AZURE_SP_OBJECT_ID
+                and principal_id_display_name
+                == f"cloudigrade-{settings.CLOUDIGRADE_ENVIRONMENT}"
+                and role_definition_id == azure.AZURE_READER_ROLE_ID
+            ):
+                logger.info(
+                    _(
+                        "Found matching managed service for deleting"
+                        " the lighthouse registration "
+                        "name=%(name)s, "
+                        "properties=%(properties)s, "
+                        "managed_by_tenant_id=%(managed_by_tenant_id)s, "
+                        "principal_id=%(principal_id)s, "
+                        "principal_id_display_name=%(principal_id_display_name)s, "
+                        "role_definition_id=%(role_definition_id)s",
+                    ),
+                    {
+                        "name": name,
+                        "properties": properties,
+                        "managed_by_tenant_id": managed_by_tenant_id,
+                        "principal_id": principal_id,
+                        "principal_id_display_name": principal_id_display_name,
+                        "role_definition_id": role_definition_id,
+                    },
+                )
+                registration_name = name
+                break
+    except Exception as e:
+        logger.error(
+            _(
+                "Unexpected error while trying to"
+                " find the lighthouse registration name: %s"
+            ),
+            e,
+        )
+        return
+
+    if not found_rd:
+        logger.info(_("No lighthouse registration definitions were found"))
+        return
+
+    if not registration_name:
+        logger.info(_("Could not find a matching lighthouse registration"))
+        return
+
+    _delete_managed_service(ms_client, ms_scope, registration_name)
+
+
+def _delete_managed_service(ms_client, ms_scope, registration_name):
+    """Delete the managed service assignment and definition."""
+    # First, delete the assignment
+    logger.info(
+        _(
+            "Attempting to delete the managed service assignment for %s",
+            registration_name,
+        )
+    )
+    try:
+        ms_client.registration_assignments.begin_delete(
+            scope=ms_scope, registration_assignment_id=registration_name
+        ).wait()
+    except Exception as e:
+        logger.info(
+            _(
+                "Unexpected error while deleting"
+                " the lighthouse registration assignment: %s"
+            ),
+            e,
+        )
+        return
+
+    # Finally, delete the definition
+    logger.info(
+        _(
+            "Attempting to delete the managed service definition for %s",
+            registration_name,
+        )
+    )
+
+    try:
+        ms_client.registration_definitions.delete(
+            scope=ms_scope, registration_definition_id=registration_name
+        )
+    except Exception as e:
+        logger.error(
+            _(
+                "Unexpected error while"
+                " deleting the lighthouse registration definition: %s"
+            ),
+            e,
         )
