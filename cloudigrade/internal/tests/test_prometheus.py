2ac7a60a87d15e9e0ad21706401d491acd04ac88
Brad Smith <brasmith@redhat.com>
2022-07-26 11:48:32 -0400

    simplify sqs queue length prometheus metrics; use one metric with multiple labels

--- cloudigrade/internal/tests/test_prometheus.py
+++ cloudigrade/internal/tests/test_prometheus.py
@@ -10,32 +10,31 @@ _faker = faker.Faker()
 
 class InternalPrometheusTestCase(TestCase):
     """
-    Test internal.prometheus.initialize_cached_metrics.
+    Test internal.prometheus.CachedMetricsRegistry.
 
-    This is a difficult and strange thing to test because normally a call to
-    initialize_cached_metrics happens in InternalAppConfig.ready, but it appears to be
-    impossible (within reason) to patch that behavior so that we override or mock it
-    before Django tests actually run. Unfortunately, the AppConfig.ready functions are
-    all called long before the test classes are loaded.
+    This is a difficult and strange thing to test because normally a call to initialize
+    happens in InternalAppConfig.ready, but it appears to be impossible (within reason)
+    to patch that behavior so that we override or mock it before Django tests actually
+    run. Unfortunately, the AppConfig.ready functions are all called long before the
+    test classes are loaded.
 
     So, we assume that InternalAppConfig.ready() may *already* have been called before
     these test functions even begin their setup.
     """
 
-    def test_initialize_cached_metrics(self):
+    def test_initialize(self):
         """
-        Test initialize_cached_metrics creates the expected Gauge objects.
+        Test initialize creates the expected Gauge object.
 
         Since we assume that "ready" may already have been called, we don't actually
-        need to call initialize_cached_metrics. We just assert the expected side-effects
-        of calling initialize_cached_metrics.
+        need to call initialize. We just assert the expected side-effects of calling
+        initialize.
         """
         from internal import prometheus
         from prometheus_client import registry
 
         self.assertEqual(
-            len(prometheus.CACHED_GAUGE_METRICS_INFO),
-            len(prometheus.CachedMetricsRegistry().get_registered_metrics_names()),
+            len(prometheus.CachedMetricsRegistry().get_registered_metrics_names()), 1
         )
 
         # Yes, _collector_to_names is pseudo-protected, but I couldn't find any other
@@ -43,12 +42,19 @@ class InternalPrometheusTestCase(TestCase):
         registered_names = itertools.chain(
             *list(registry.REGISTRY._collector_to_names.values())
         )
-        for info in prometheus.CACHED_GAUGE_METRICS_INFO:
-            self.assertIn(info.metric_name, registered_names)
+        self.assertIn(prometheus.CACHED_SQS_QUEUE_LENGTH_METRIC_NAME, registered_names)
+        # Ditto for _names_to_collectors being protected. I wish I could find a better
+        # supported mechanism to access the registry's internals.
+        individual_metrics = registry.REGISTRY._names_to_collectors[
+            prometheus.CACHED_SQS_QUEUE_LENGTH_METRIC_NAME
+        ]._metrics
+        self.assertEqual(
+            len(individual_metrics), len(prometheus.CACHED_SQS_QUEUE_LENGTH_LABELS)
+        )
 
-    def test_initialize_cached_metrics_multiple_calls(self):
+    def test_initialize_multiple_calls(self):
         """
-        Test initialize_cached_metrics only creates the Gauge objects once.
+        Test initialize only creates the Gauge object once.
 
         Since we assume that "ready" may already have been called, we patch two things:
         the _gauge_metrics dict that contains known created metrics and the
@@ -64,8 +70,6 @@ class InternalPrometheusTestCase(TestCase):
         ), patch("internal.prometheus.Gauge") as mock_gauge:
             from internal import prometheus
 
-            expected_final_count = len(prometheus.CACHED_GAUGE_METRICS_INFO)
-
             registry = prometheus.CachedMetricsRegistry()
 
             # Initially _gauge_metrics should be empty.
@@ -73,9 +77,9 @@ class InternalPrometheusTestCase(TestCase):
 
             registry.initialize()
             metrics_after_first_call = registry.get_registered_metrics_names()
-            # After one call, _gauge_metrics should be fully loaded.
-            self.assertEqual(expected_final_count, len(metrics_after_first_call))
-            self.assertEqual(expected_final_count, mock_gauge.call_count)
+            # After one call, _gauge_metrics should be populated.
+            self.assertEqual(1, len(metrics_after_first_call))
+            mock_gauge.assert_called_once()
 
             mock_gauge.reset_mock()
 
@@ -83,9 +87,9 @@ class InternalPrometheusTestCase(TestCase):
             # After the second call, _gauge_metrics should be unchanged,
             # and there should be no more Gauge calls.
             metrics_after_second_call = registry.get_registered_metrics_names()
-            self.assertEqual(expected_final_count, len(metrics_after_second_call))
+            self.assertEqual(1, len(metrics_after_second_call))
             self.assertEqual(metrics_after_first_call, metrics_after_second_call)
-            self.assertEqual(0, mock_gauge.call_count)
+            mock_gauge.assert_not_called()
 
     def test_initialize_celery_queue_length_metrics(self):
         """
