cloudigrade/cloudigrade

commit f91d9c0a918211f9228013ba5cd29a8edc01d99c

move redis command execution to reusable helper function internal.redis.execute_command

cloudigrade/internal/views.py
@@ -6,7 +6,6 @@ from dateutil.parser import parse
 from django.core.cache import cache
 from django.http import Http404, JsonResponse
 from django.utils.translation import gettext as _
-from django_redis import get_redis_connection
 from rest_framework import exceptions, permissions, status
 from rest_framework.decorators import (
     api_view,
@@ -21,7 +20,7 @@ from rest_framework.views import APIView
 from api import models, tasks
 from api.tasks import enable_account
 from api.util import find_problematic_runs
-from internal import serializers
+from internal import redis, serializers
 from internal.authentication import (
     IdentityHeaderAuthenticationInternal,
 )
@@ -425,9 +424,7 @@ def redis_raw(request):
         args = serializer.validated_data["args"].split(" ")
 
         try:
-            with get_redis_connection("default") as connection:
-                func = getattr(connection, command)
-                results = func(*args)
+            results = redis.execute_command(command, *args)
         except TypeError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:

cloudigrade/internal/tests/views/test_redis_raw.py
@@ -18,7 +18,7 @@ class RedisRawViewTest(TestCase):
     def setUp(self):
         """Set up shared test data."""
         self.factory = APIRequestFactory()
-        get_redis_connection_patch = patch("internal.views.get_redis_connection")
+        get_redis_connection_patch = patch("internal.views.redis.get_redis_connection")
         mock_get_redis_connection = get_redis_connection_patch.start()
         self.mock_connection = (
             mock_get_redis_connection.return_value.__enter__.return_value

cloudigrade/internal/redis.py
@@ -1,0 +1,10 @@
+"""Internal Redis helper functions."""
+from django_redis import get_redis_connection
+
+
+def execute_command(command, args):
+    """Execute a Redis command."""
+    with get_redis_connection("default") as connection:
+        func = getattr(connection, command)
+        results = func(*args)
+    return results

commit 5a34a62dab3bd1cb75adfa6f92c0c4195d9787fa

fix typo introduced by f91d9c0a918211f9228013ba5cd29a8edc01d99c and update unit test to verify the behavior more explicitly

cloudigrade/internal/views.py
@@ -424,7 +424,7 @@ def redis_raw(request):
         args = serializer.validated_data["args"].split(" ")
 
         try:
-            results = redis.execute_command(command, *args)
+            results = redis.execute_command(command, args)
         except TypeError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:

cloudigrade/internal/tests/views/test_redis_raw.py
@@ -36,9 +36,10 @@ class RedisRawViewTest(TestCase):
         command = _faker.random_element(
             InternalRedisRawInputSerializer.allowed_commands
         )
-        args = _faker.word()
+        individual_args = [_faker.word(), _faker.word()]
+        args_string = " ".join(individual_args)
         request = self.factory.post(
-            "/redis_raw/", data={"command": command, "args": args}, format="json"
+            "/redis_raw/", data={"command": command, "args": args_string}, format="json"
         )
 
         redis_command_results = _faker.sentence()
@@ -48,6 +49,7 @@ class RedisRawViewTest(TestCase):
         expected_response_data = {"results": redis_command_results}
 
         response = redis_raw(request)
+        mock_func.assert_called_with(*individual_args)
         self.assertEqual(response.status_code, 200)
         self.assertExpectedJsonResponse(response, expected_response_data)
 