6ca45c694adb1d96795f6f7c9613c6f7361a3bfa
Brad Smith <brasmith@redhat.com>
2022-10-06 16:15:13 -0400

    make wait_for_migrations.sh significantly less noisy

--- deployment/scripts/wait_for_migrations.sh
+++ deployment/scripts/wait_for_migrations.sh
@@ -2,7 +2,7 @@
 # Wait until the database is reachable and all Django migrations have completed.
 
 waitForMigrations() {
-    while ! python3 ./manage.py migrate --plan --check; do
+    while ! CLOUDIGRADE_LOG_LEVEL=ERROR python3 ./manage.py migrate --check; do
         echo "$(date '+%Y-%m-%dT%H.%M.%S') WARNING Database migrations have not yet completed." >&2
         sleep 5
     done
