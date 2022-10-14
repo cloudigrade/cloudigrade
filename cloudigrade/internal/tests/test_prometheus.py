471ac6cfe95f8ce83eb1ee49767012c095d3698a
Alberto Bellotti <abellott@redhat.com>
2022-10-14 03:21:25 -0400

    Adding tests for the new internal.prometheus table metrics.

--- cloudigrade/internal/tests/test_prometheus.py
+++ cloudigrade/internal/tests/test_prometheus.py
@@ -1,6 +1,6 @@
 """Collection of tests for the internal.prometheus module."""
 import itertools
-from unittest.mock import call, patch
+from unittest.mock import MagicMock, call, patch
 
 import faker
 from django.test import TestCase, override_settings
@@ -125,6 +125,99 @@ class InternalPrometheusTestCase(TestCase):
             registry._initialize_celery_queue_length_metrics()
             self.assertIn(
                 prometheus.CELERY_QUEUE_LENGTH_METRIC_NAME,
+                registry.get_registered_metrics_names(),
+            )
+            mock_gauge.assert_called_once()
+            mock_gauge_instance = mock_gauge.return_value
+            actual_labels_calls = mock_gauge_instance.labels.mock_calls
+            for expected_labels_call in expected_labels_calls:
+                self.assertIn(expected_labels_call, actual_labels_calls)
+
+    def test_initialize_db_table_size_metrics(self):
+        """
+        Test Database table size metrics are created as expected.
+
+        Normally the _initialize_db_table_size_metrics function never creates the
+        prometheus gauge by unit tests because unit tests are not configured to
+        use a Postgres database. This test short-circuits the database check and
+        sql statement executions to make metrics initialization possible.
+        """
+        fake_table_sizes = [
+            [_faker.slug(), 1024 * _faker.random_int(min=1, max=256)],
+            [_faker.slug(), 1024 * _faker.random_int(min=1, max=256)],
+        ]
+        expected_labels_calls = [
+            call(table_name=table_name) for table_name, table_size in fake_table_sizes
+        ]
+
+        patched_custom_gauge_metrics = {}
+        with patch(
+            "internal.prometheus.CachedMetricsRegistry._gauge_metrics",
+            patched_custom_gauge_metrics,
+        ), patch("internal.prometheus.Gauge") as mock_gauge, patch(
+            "internal.prometheus.CachedMetricsRegistry.db_is_pg",
+        ) as mock_db_is_pg, patch(
+            "internal.prometheus.connection"
+        ) as mock_connection:
+            mock_db_is_pg.return_value = True
+            mock_cursor = MagicMock()
+            mock_connection.cursor.return_value = mock_cursor
+            mock_cursor.execute.return_value = None
+            mock_cursor.fetchall.return_value = fake_table_sizes
+
+            from internal import prometheus
+
+            registry = prometheus.CachedMetricsRegistry()
+            registry._initialize_db_table_size_metrics()
+            self.assertIn(
+                prometheus.DB_TABLE_SIZE_METRIC_NAME,
+                registry.get_registered_metrics_names(),
+            )
+            mock_gauge.assert_called_once()
+            mock_gauge_instance = mock_gauge.return_value
+            actual_labels_calls = mock_gauge_instance.labels.mock_calls
+            for expected_labels_call in expected_labels_calls:
+                self.assertIn(expected_labels_call, actual_labels_calls)
+
+    def test_initialize_db_table_rows_metrics(self):
+        """
+        Test Database table rows metrics are created as expected.
+
+        Normally the _initialize_db_table_rows_metrics function never creates the
+        prometheus gauge by unit tests because unit tests are not configured to
+        use a Postgres database. This test short-circuits the database check and
+        sql statement executions to make metrics initialization possible.
+        """
+        fake_table_rows = [
+            [_faker.slug(), _faker.random_int(min=1, max=16384)],
+            [_faker.slug(), _faker.random_int(min=1, max=16384)],
+            [_faker.slug(), _faker.random_int(min=1, max=16384)],
+        ]
+        expected_labels_calls = [
+            call(table_name=table_name) for table_name, table_rows in fake_table_rows
+        ]
+
+        patched_custom_gauge_metrics = {}
+        with patch(
+            "internal.prometheus.CachedMetricsRegistry._gauge_metrics",
+            patched_custom_gauge_metrics,
+        ), patch("internal.prometheus.Gauge") as mock_gauge, patch(
+            "internal.prometheus.CachedMetricsRegistry.db_is_pg",
+        ) as mock_db_is_pg, patch(
+            "internal.prometheus.connection"
+        ) as mock_connection:
+            mock_db_is_pg.return_value = True
+            mock_cursor = MagicMock()
+            mock_connection.cursor.return_value = mock_cursor
+            mock_cursor.execute.return_value = None
+            mock_cursor.fetchall.return_value = fake_table_rows
+
+            from internal import prometheus
+
+            registry = prometheus.CachedMetricsRegistry()
+            registry._initialize_db_table_rows_metrics()
+            self.assertIn(
+                prometheus.DB_TABLE_ROWS_METRIC_NAME,
                 registry.get_registered_metrics_names(),
             )
             mock_gauge.assert_called_once()
