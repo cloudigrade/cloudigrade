1d8b8d4dbfdb76513325abdba6e5ffc39c0a5023
Alberto Bellotti <abellott@redhat.com>
2022-11-15 17:26:43 -0500

    Adding unit tests
    - Creating new machine images with vm data including rhel and openshift
      detected flags.
    - Adding concurrent usage tests that include azure machine images

--- cloudigrade/api/tests/clouds/azure/util/test_create_new_machine_images.py
+++ cloudigrade/api/tests/clouds/azure/util/test_create_new_machine_images.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 
 from api.clouds.azure import util
 from api.clouds.azure.models import AzureMachineImage
+from api.models import MachineImage
+from util import OPENSHIFT_TAG, RHEL_TAG
 from util.tests import helper as util_helper
 
 _faker = faker.Faker()
@@ -32,4 +34,58 @@ class CreateAzureNewMachineImages(TestCase):
         )
         self.assertTrue(
             AzureMachineImage.objects.filter(resource_id=vm2["image_sku"]).exists()
+        )
+
+    def test_create_new_machine_images_rhel_detected(self):
+        """Tests creation of a machine image that is tagged as RHEL."""
+        vm1 = util_helper.generate_vm_data(tags={RHEL_TAG: 1})
+        new_skus = util.create_new_machine_images([vm1])
+        self.assertSetEqual(set(new_skus), {vm1["image_sku"]})
+        self.assertTrue(AzureMachineImage.objects.count() == 1)
+        self.assertTrue(
+            AzureMachineImage.objects.filter(resource_id=vm1["image_sku"]).exists()
+        )
+        self.assertTrue(MachineImage.objects.count() == 1)
+        self.assertTrue(
+            MachineImage.objects.filter(
+                name=vm1["image_sku"],
+                rhel_detected_by_tag=True,
+                openshift_detected=False,
+            ).exists()
+        )
+
+    def test_create_new_machine_images_openshift_detected(self):
+        """Tests creation of a machine image that is tagged as openshift."""
+        vm1 = util_helper.generate_vm_data(tags={OPENSHIFT_TAG: 1})
+        new_skus = util.create_new_machine_images([vm1])
+        self.assertSetEqual(set(new_skus), {vm1["image_sku"]})
+        self.assertTrue(AzureMachineImage.objects.count() == 1)
+        self.assertTrue(
+            AzureMachineImage.objects.filter(resource_id=vm1["image_sku"]).exists()
+        )
+        self.assertTrue(MachineImage.objects.count() == 1)
+        self.assertTrue(
+            MachineImage.objects.filter(
+                name=vm1["image_sku"],
+                rhel_detected_by_tag=False,
+                openshift_detected=True,
+            ).exists()
+        )
+
+    def test_create_new_machine_images_rhel_and_openshift_detected(self):
+        """Tests creation of a machine image that is tagged as rhel and openshift."""
+        vm1 = util_helper.generate_vm_data(tags={OPENSHIFT_TAG: 1, RHEL_TAG: 1})
+        new_skus = util.create_new_machine_images([vm1])
+        self.assertSetEqual(set(new_skus), {vm1["image_sku"]})
+        self.assertTrue(AzureMachineImage.objects.count() == 1)
+        self.assertTrue(
+            AzureMachineImage.objects.filter(resource_id=vm1["image_sku"]).exists()
+        )
+        self.assertTrue(MachineImage.objects.count() == 1)
+        self.assertTrue(
+            MachineImage.objects.filter(
+                name=vm1["image_sku"],
+                rhel_detected_by_tag=True,
+                openshift_detected=True,
+            ).exists()
         )
