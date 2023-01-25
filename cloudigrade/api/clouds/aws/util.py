14c1a2bb6a1fffbab3a1a139ed2c8c342198e14b
Brad Smith <brasmith@redhat.com>
2023-01-25 13:36:52 -0500

    stop powering off instances when disabling a cloud account

--- cloudigrade/api/clouds/aws/util.py
+++ cloudigrade/api/clouds/aws/util.py
@@ -316,7 +316,7 @@ def update_aws_cloud_account(
         # We don't need to notify sources here because we're just about to create the
         # new replacement CloudAccount which will also notify sources when enabled, and
         # we already have explicit calls to notify if we can't create the CloudAccount.
-        cloud_account.disable(power_off_instances=False, notify_sources=False)
+        cloud_account.disable(notify_sources=False)
 
         # Remove instances associated with the cloud account
         # TODO instead of deleting instances here, delete the original CloudAccount.
