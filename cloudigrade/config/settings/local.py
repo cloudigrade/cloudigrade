fe9d86453290ec234aa36b9a5bf78c7a8e147d6c
Brad Smith <brasmith@redhat.com>
2023-02-01 15:19:27 -0500

    remove unused dependencies jupyter, ipython, and django-extensions

--- cloudigrade/config/settings/local.py
+++ cloudigrade/config/settings/local.py
@@ -3,5 +3,3 @@ from .base import *
 
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 SECRET_KEY = env("DJANGO_SECRET_KEY", default="local")
-if env.bool("ENABLE_DJANGO_EXTENSIONS", default=False):
-    INSTALLED_APPS = INSTALLED_APPS + ["django_extensions"]
