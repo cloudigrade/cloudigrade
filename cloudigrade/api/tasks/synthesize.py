029ffe8c166f01fe5ce3a49c2f346cd4b9ac815b
Brad Smith <brasmith@redhat.com>
2023-02-02 12:29:20 -0500

    remove unused task synthesize_concurrent_usage

--- cloudigrade/api/tasks/synthesize.py
+++ cloudigrade/api/tasks/synthesize.py
@@ -67,12 +67,3 @@ def synthesize_runs_and_usage(request_id: int) -> Optional[int]:
     This is a placeholder for old in-flight tasks during the shutdown transition.
     """
     # TODO FIXME Delete this function once we're confident no tasks exists.
-
-
-@shared_task(name="api.tasks.synthesize_concurrent_usage", serializer="pickle")
-def synthesize_concurrent_usage(__: object, user_id: int, on_date: date) -> int:
-    """Do nothing.
-
-    This is a placeholder for old in-flight tasks during the shutdown transition.
-    """
-    # TODO FIXME Delete this function once we're confident no tasks exists.
