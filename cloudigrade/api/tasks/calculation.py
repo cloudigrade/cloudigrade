23ef9637da04e033c4357ec9e002cabf29391e6e
Brad Smith <brasmith@redhat.com>
2023-02-02 12:26:59 -0500

    remove unused task recalculate_concurrent_usage_for_all_users

--- cloudigrade/api/tasks/calculation.py
+++ cloudigrade/api/tasks/calculation.py
@@ -56,12 +56,3 @@ def recalculate_concurrent_usage_for_user_id(user_id, since=None):
     This is a placeholder for old in-flight tasks during the shutdown transition.
     """
     # TODO FIXME Delete this function once we're confident no tasks exists.
-
-
-@shared_task(name="api.tasks.recalculate_concurrent_usage_for_all_users")
-def recalculate_concurrent_usage_for_all_users(since=None):
-    """Do nothing.
-
-    This is a placeholder for old in-flight tasks during the shutdown transition.
-    """
-    # TODO FIXME Delete this function once we're confident no tasks exists.
