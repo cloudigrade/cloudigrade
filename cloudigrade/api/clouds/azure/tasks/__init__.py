3d79b5402794ffebd2e37add8a6d5f9a275153d2
Brad Smith <brasmith@redhat.com>
2023-02-02 12:58:06 -0500

    remove unused task initial_azure_vm_discovery

--- cloudigrade/api/clouds/azure/tasks/__init__.py
+++ cloudigrade/api/clouds/azure/tasks/__init__.py
@@ -26,5 +26,4 @@ decorator even if the function itself is renamed or moved elsewhere.
 from api.clouds.azure.tasks.maintenance import repopulate_azure_instance_mapping
 from api.clouds.azure.tasks.onboarding import (
     check_azure_subscription_and_create_cloud_account,
-    initial_azure_vm_discovery,
 )
