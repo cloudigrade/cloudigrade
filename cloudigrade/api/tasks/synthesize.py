d8a39fe74165cad16fb8fa396d98c4357f47ff45
Brad Smith <brasmith@redhat.com>
2023-02-02 12:32:25 -0500

    remove unused task synthesize_images

--- cloudigrade/api/tasks/synthesize.py
+++ cloudigrade/api/tasks/synthesize.py
@@ -31,12 +31,3 @@ def synthesize_cloud_accounts(request_id: int) -> Optional[int]:
     This is a placeholder for old in-flight tasks during the shutdown transition.
     """
     # TODO FIXME Delete this function once we're confident no tasks exists.
-
-
-@shared_task(name="api.tasks.synthesize_images")
-def synthesize_images(request_id: int) -> Optional[int]:
-    """Do nothing.
-
-    This is a placeholder for old in-flight tasks during the shutdown transition.
-    """
-    # TODO FIXME Delete this function once we're confident no tasks exists.
