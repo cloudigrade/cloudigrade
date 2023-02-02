ddf9897b691332c7668e5e3738ddf2e935581d9b
Brad Smith <brasmith@redhat.com>
2023-02-02 12:35:39 -0500

    remove unused task recalculate_runs_for_all_cloud_accounts

--- cloudigrade/api/tasks/calculation.py
+++ cloudigrade/api/tasks/calculation.py
@@ -18,12 +18,3 @@ def recalculate_runs_for_cloud_account_id(cloud_account_id, since=None):
     This is a placeholder for old in-flight tasks during the shutdown transition.
     """
     # TODO FIXME Delete this function once we're confident no tasks exists.
-
-
-@shared_task(name="api.tasks.recalculate_runs_for_all_cloud_accounts")
-def recalculate_runs_for_all_cloud_accounts(since=None):
-    """Do nothing.
-
-    This is a placeholder for old in-flight tasks during the shutdown transition.
-    """
-    # TODO FIXME Delete this function once we're confident no tasks exists.
