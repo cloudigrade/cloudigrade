803ae61455a0710bcca412543163a3290a9cfeae
Henning Seljenes <hseljene@redhat.com>
2024-10-02 11:17:24 -0400

    Drop --cache-from in deployment/scripts/build_deploy.sh

--- deployment/scripts/build_deploy.sh
+++ deployment/scripts/build_deploy.sh
@@ -22,7 +22,7 @@ docker --config="${DOCKER_CONF}" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_T
 # Pull 'Latest' Image
 docker --config="${DOCKER_CONF}" pull ${IMAGE_NAME}:latest || true
 # Build and Tag
-docker --config="${DOCKER_CONF}" build --cache-from ${IMAGE_NAME}:latest --tag ${IMAGE_NAME}:${IMAGE_TAG} .
+docker --config="${DOCKER_CONF}" build --tag ${IMAGE_NAME}:${IMAGE_TAG} .
 docker --config="${DOCKER_CONF}" tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 # Push images
 docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:latest"
