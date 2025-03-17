a9f4ff3ea585c70c5cae4b18be55502425fbc478
Henning Seljenes <hseljene@redhat.com>
2025-03-17 15:46:07 -0400

    IQE_PARALLEL_ENABLED=false

--- deployment/scripts/pr_check.sh
+++ deployment/scripts/pr_check.sh
@@ -11,6 +11,7 @@ export IQE_PLUGINS="cloudmeter"  # name of the IQE plugin for this APP
 export IQE_MARKER_EXPRESSION="smoke"  # This is the value passed to pytest -m
 export IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
 export IQE_CJI_TIMEOUT="23m"  # This is the time to wait for smoke test to complete or fail
+export IQE_PARALLEL_ENABLED="false" # Disable parallel to avoid OOMing
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
